c7565ff63cbe79fdd3d9cdd31b472d98573ce175
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-04-21 12:25:42 -0700

    Runtime: 36 ms (46.80%), Memory: 16.4 MB (83.88%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,15 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
         # dp being the n-1 + n-2 as both can reach stair n
-        dpList = list()
+        # dpList = list()
         # at step 0 is 1 while step 1 is 1
-        dpList.append(1)
-        dpList.append(1)
+        # dpList.append(1)
+        # dpList.append(1)
+        # for i in range(2, n+1, 1):
+        #     dpList.append(dpList[i-1]+dpList[i-2])
+        dp = [0]*(n+1)
+        dp[0] = 1
+        dp[1] = 1
+        if n < 2:
+            return dp[n]
         for i in range(2, n+1, 1):
-            dpList.append(dpList[i-1]+dpList[i-2])
-        return dpList[n]
+            dp[i] = dp[i-2]+dp[i-1]
+        return dp[n]
         
 
 
