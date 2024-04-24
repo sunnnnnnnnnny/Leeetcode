de69324ae3d4a88138c09dadefef008f01fd37f6
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-04-23 23:56:56 -0700

    Runtime: 36 ms (54.56%), Memory: 16.5 MB (89.75%) - Gitcode

---
+++ 198-house-robber/198. House Robber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # dp[i] = max(nums[i]+dp[i+2], dp[i+11])
+        # botton-up
+        # dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+        if len(nums)==1:
+            return nums[0]
+        dpMoney = ([0]*len(nums))
+        dpMoney[0] = nums[0]
+        dpMoney[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums), 1):
+            dpMoney[i] = max(nums[i]+dpMoney[i-2], dpMoney[i-1])
+        return dpMoney[len(nums)-1]
