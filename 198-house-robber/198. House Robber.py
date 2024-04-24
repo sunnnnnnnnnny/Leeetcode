99c70e2d7d15e2d229b9a7285f383a705254c9fc
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-04-23 23:59:54 -0700

    Runtime: 34 ms (72.56%), Memory: 16.6 MB (12.18%) - Gitcode

--- 198-house-robber/198. House Robber.py
+++ 198-house-robber/198. House Robber.py
@@ -8,6 +8,8 @@ class Solution:
         dpMoney = ([0]*len(nums))
         dpMoney[0] = nums[0]
         dpMoney[1] = max(nums[0], nums[1])
+        # could replace the dpMoney List as only recording the prev 2 nums
+        # thus the space can be saved from O(N)->O(1)
         for i in range(2, len(nums), 1):
             dpMoney[i] = max(nums[i]+dpMoney[i-2], dpMoney[i-1])
         return dpMoney[len(nums)-1]
