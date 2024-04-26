28f3cbd7ba397bde6f6d2a6bf6d1034fb7fedecb
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-04-25 21:00:10 -0700

    Runtime: 33 ms (77.46%), Memory: 16.5 MB (33.63%) - Gitcode

--- 213-house-robber-ii/213. House Robber II.py
+++ 213-house-robber-ii/213. House Robber II.py
@@ -10,7 +10,6 @@ class Solution:
                 return max(nums[start], nums[end])
             # print('start: end:',start, end)
             return max(nums[end]+dp(end-2,start), dp(end-1, start))
-        
         def dp_simple(self, nums:List[int])-> int:
             if len(nums) == 0:
                 return 0
@@ -29,6 +28,7 @@ class Solution:
             return nums[n]
         # print(nums[1:n+1])
         # print(nums[0:n])
+        # to break the tie of connected cycle of first and last house
         dp_n_to_1 = dp_simple(self, nums[1:n+1])
         dp_nm1_to_0 = dp_simple(self, nums[0:n])
         return max(dp_n_to_1, dp_nm1_to_0)
