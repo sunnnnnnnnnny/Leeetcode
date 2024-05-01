7bf206180deae7fea37efa3568b8ca360664a8f3
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-04-30 23:15:18 -0700

    Runtime: 1034 ms (80.82%), Memory: 55.4 MB (29.90%) - Gitcode

--- 435-non-overlapping-intervals/435. Non-overlapping Intervals.py
+++ 435-non-overlapping-intervals/435. Non-overlapping Intervals.py
@@ -3,8 +3,13 @@ class Solution:
         # sort by ending interval
         # greedy picking the earlier ending one interval one
         # the same question of maximum none overlapping interval
+        # time: O(NlogN+N)=> O(nlogn) sort+traversal
+        # space: O(N) b/c the sorting of Timesort in python
         intervals.sort(key=lambda x:x[1])
-        nowEnd = intervals[0][0]-1
+        # the first ending should be not overlapped with the first interval
+        # could also use -inf
+        # nowEnd = intervals[0][0]-1
+        nowEnd = -inf
         ret = 0
         for interval in intervals:
             if nowEnd<=interval[0]:
