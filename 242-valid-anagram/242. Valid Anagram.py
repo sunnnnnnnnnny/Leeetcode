690b09e7f6e6a1499b59ad9b41540de414f4f5c4
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-05-06 09:49:02 -0700

    Runtime: 64 ms (6.06%), Memory: 17 MB (33.98%) - Gitcode

---
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # By adding the counts of character in s
+        # then check if all the character appeared the same freq in t
+        # time: O(s+t) as we would explore the whole s and t string
+        # space: O(s) as we keep record max of all the char in s
+        if len(s)!=len(t):
+            return False
+
+        sChar = {}
+        for i in range(len(s)):
+            if s[i] not in sChar:
+                sChar[s[i]] = 0
+            sChar[s[i]] += 1
+        
+        for i in range(len(t)):
+            if t[i] not in sChar:
+                return False
+            sChar[t[i]] -= 1
+            if sChar[t[i]] == 0:
+                del sChar[t[i]]
+            
+        return True
+
+
+        
