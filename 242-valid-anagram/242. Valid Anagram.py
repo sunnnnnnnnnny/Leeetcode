7acc2a8543af6c7449a9f73f4767aa48553cc6a3
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-05-06 09:53:33 -0700

    Runtime: 44 ms (80.95%), Memory: 17.1 MB (33.98%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -4,23 +4,27 @@ class Solution:
         # then check if all the character appeared the same freq in t
         # time: O(s+t) as we would explore the whole s and t string
         # space: O(s) as we keep record max of all the char in s
+        # method 2, by sorting the s and t to compare them
+        # time: O(NlogN) space: O(1)
         if len(s)!=len(t):
             return False
-
-        sChar = {}
-        for i in range(len(s)):
-            if s[i] not in sChar:
-                sChar[s[i]] = 0
-            sChar[s[i]] += 1
+        # by utilizing the counter of python
+        from collections import Counter 
+        return Counter(s) == Counter(t)
+        # sChar = {}
+        # for i in range(len(s)):
+        #     if s[i] not in sChar:
+        #         sChar[s[i]] = 0
+        #     sChar[s[i]] += 1
         
-        for i in range(len(t)):
-            if t[i] not in sChar:
-                return False
-            sChar[t[i]] -= 1
-            if sChar[t[i]] == 0:
-                del sChar[t[i]]
+        # for i in range(len(t)):
+        #     if t[i] not in sChar:
+        #         return False
+        #     sChar[t[i]] -= 1
+        #     if sChar[t[i]] == 0:
+        #         del sChar[t[i]]
             
-        return True
+        # return True
 
 
         
