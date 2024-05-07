7a7221de3f0364182bc35ce56d0d6fd962d8c559
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-05-06 22:15:54 -0700

    Runtime: 48 ms (5.47%), Memory: 16.6 MB (74.73%) - Gitcode

---
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # using FILO queue to record the open bracket
+        # once meeting the close bracket, pop the last open one
+        # check if the type is correct, and at the end if open queue>0
+        # TIme; O(N) traverse the str with each pop cost O(1)
+        # Space:O(N) max all open bracket goes into the queue
+        # or convert the open bracket as close brackt str, 
+        # and move the pointers when meeting one close, to verify the validtion
+        # Time: O(N) Space:O(N)
+        open = deque()
+        def getCloseBracket(char):
+            if char == '(' :
+                return ')'
+            elif char =='{' :
+                return '}'
+            elif char == '[':
+                return ']'
+            return ''
+        for char in s:
+            if char == '(' or char =='{' or char == '[':
+                open.append(char)
+            elif char == ')' or char == '}' or char == ']':
+                if len(open)==0:
+                    return False
+                lastOpen = open.pop()
+                shouldClose = getCloseBracket(lastOpen)
+                if shouldClose != char:
+                    return False
+            else:
+                return False
+        return len(open) == 0
+        
