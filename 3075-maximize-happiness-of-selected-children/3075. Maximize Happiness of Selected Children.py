81fff93da1282a5a97405ef3246d149e049f71f9
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-05-08 23:37:04 -0700

    Runtime: 805 ms (59.95%), Memory: 43.7 MB (8.28%) - Gitcode

--- 3075-maximize-happiness-of-selected-children/3075. Maximize Happiness of Selected Children.py
+++ 3075-maximize-happiness-of-selected-children/3075. Maximize Happiness of Selected Children.py
@@ -3,14 +3,22 @@ class Solution:
         # since all child will decrease happiness after each turn
         # thus greedy select the top K value of the happiness child
         # then minus the k-1 turns of decreament: 
-        # time: O(N+K) building the max heap and pop K items
+        # time: O(N+KlogN) building the max heap O(N) and pop K items with each cost O(logN)
         # space: O(N) of the heap
-        maxKItem = heapq.nlargest(k, happiness)
+        # python default is min heap
+        maxKItem = [-h for h in happiness]
+        heapq.heapify(maxKItem)
         ret = 0
-        for idx in range(len(maxKItem)):
-            nowHappy = maxKItem[idx]-idx if maxKItem[idx]-idx>0 else 0
-            if nowHappy == 0:
+        for i in range(k):
+            nowHappy = -heapq.heappop(maxKItem)
+            curHappy = nowHappy-i if nowHappy-i>0 else 0
+            if curHappy == 0:
                 break
-            ret += nowHappy
+            ret += curHappy
+        # for idx in range(len(maxKItem)):
+        #     nowHappy = maxKItem[idx]-idx if maxKItem[idx]-idx>0 else 0
+        #     if nowHappy == 0:
+        #         break
+        #     ret += nowHappy
         return ret
         
