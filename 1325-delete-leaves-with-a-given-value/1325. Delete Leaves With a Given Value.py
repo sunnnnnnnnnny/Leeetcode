2ad948c72a861082beb82e0d6b65d38831f4df2d
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-05-24 14:54:31 -0700

    Runtime: 45 ms (58.65%), Memory: 17.1 MB (75.10%) - Gitcode

--- 1325-delete-leaves-with-a-given-value/1325. Delete Leaves With a Given Value.py
+++ 1325-delete-leaves-with-a-given-value/1325. Delete Leaves With a Given Value.py
@@ -8,17 +8,15 @@ class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
         # using dfs traverse the tree, and return the child node for update
         # time: O(N) N is tree nodes
-        # Space: O(1) no extra space is needed
+        # Space: O(N) as the call stack will be used
         def getChild(now: Optional[TreeNode]):
             if now == None:
                 return now
-            newLeft = getChild(now.left)
-            newRight = getChild(now.right)
-            if newLeft == None and newRight == None:
+            now.left = getChild(now.left)
+            now.right = getChild(now.right)
+            if now.left == None and now.right == None:
                 if now.val == target:
                     return None
-            now.left = newLeft
-            now.right = newRight
             return now
         
         newRoot = getChild(root)
