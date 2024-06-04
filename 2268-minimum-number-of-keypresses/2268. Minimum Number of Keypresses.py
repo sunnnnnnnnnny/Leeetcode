aeca7de91ef5544913270346dc15906219c7fc50
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-03 23:34:13 -0700

    Runtime: 39 ms (99.67%), Memory: 17.7 MB (48.90%) - Gitcode

--- 2268-minimum-number-of-keypresses/2268. Minimum Number of Keypresses.py
+++ 2268-minimum-number-of-keypresses/2268. Minimum Number of Keypresses.py
@@ -5,22 +5,23 @@ class Solution:
         # assign the high frequency to be one hit
         # thus assign based on the freq to get the min press
         # time: O(N+NlogN+N) space:O(N)
-        charCount = {}
-        for i in range(len(s)):
-            if s[i] not in charCount.keys():
-                charCount[s[i]] = 0
-            charCount[s[i]] += 1
+        # charCount = {}
+        # for i in range(len(s)):
+        #     if s[i] not in charCount.keys():
+        #         charCount[s[i]] = 0
+        #     charCount[s[i]] += 1
+        charCount = collections.Counter(s)
         # sort the freq from high to low
-        cnts = list(charCount.values())
-        cnts.sort(reverse=True)
+        # cnts = list(charCount.values())
+        # cnts.sort(reverse=True)
         ret = 0
         keyPressAssign = 0
         # keyPressAssignLeft = 9
-        for i in range(len(cnts)):
+        for i, freq in enumerate(sorted(charCount.values(), reverse=True)):
             if i%9 == 0:
                 keyPressAssign += 1
             # keyFreq = cnts[i]
-            ret += (cnts[i]*keyPressAssign)
+            ret += (freq*keyPressAssign)
         return ret
 
 
