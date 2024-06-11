d4212d58f4e6449bd19f2e222b6943b1cf3d0b0e
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-11 09:55:42 -0700

    Runtime: 538 ms (46.25%), Memory: 31 MB (33.43%) - Gitcode

--- 53-maximum-subarray/53. Maximum Subarray.py
+++ 53-maximum-subarray/53. Maximum Subarray.py
@@ -5,18 +5,27 @@ class Solution:
         # thus getting the max sum for current loc
         # where the minSum is keep tracked by minHeap
         # Getting the Sum on the fly
-        # time: O(N)
+        # time: O(NLogN)
         # space: O(N) sumArray with minHeap
-        curSum = nums[0]
-        minHeap = []
-        maxSum = nums[0]
-        heapq.heappush(minHeap, 0)
-        heapq.heappush(minHeap, curSum)
-        for i in range(1, len(nums), 1):
-            curSum += nums[i]
-            maxCurSum = curSum - minHeap[0]
-            maxSum = max(maxSum, maxCurSum)
-            heapq.heappush(minHeap, curSum)
-        return maxSum
+        # curSum = nums[0]
+        # minHeap = []
+        # maxSum = nums[0]
+        # heapq.heappush(minHeap, 0)
+        # heapq.heappush(minHeap, curSum)
+        # for i in range(1, len(nums), 1):
+        #     curSum += nums[i]
+        #     maxCurSum = curSum - minHeap[0]
+        #     maxSum = max(maxSum, maxCurSum)
+        #     heapq.heappush(minHeap, curSum)
+        # return maxSum
+
+        # By only keeping the connected positive which is greedy
+        # thus we keep record of the cur_subarray or cur
+        # time:O(N) space:O(1)
+        curSub = maxSub = nums[0]
+        for num in nums[1:]:
+            curSub = max(num, curSub+num)
+            maxSub = max(maxSub, curSub)
+        return maxSub
 
         
