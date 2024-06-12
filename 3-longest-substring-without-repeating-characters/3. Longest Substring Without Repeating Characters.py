fd610eb603e6bcc7c0b351506c82909a7b3669a1
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-12 09:52:51 -0700

    Runtime: 52 ms (72.19%), Memory: 16.6 MB (49.38%) - Gitcode

--- 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.py
+++ 3-longest-substring-without-repeating-characters/3. Longest Substring Without Repeating Characters.py
@@ -1,28 +1,39 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        nonReap = {}
+        maxLen = 0
+        start = end = 0
+        while end<len(s):
+            if s[end] in nonReap.keys() and nonReap[s[end]]>=start:
+                start = nonReap[s[end]]+1
+            nonReap[s[end]] = end
+            # print(s[end], start, end)
+            maxLen = max(maxLen, end-start+1)
+            end += 1
+        return maxLen
         # using dict to record the nonrepeat char with their idx
         # the len of the set is the cnt
         # Time: O(N) space:O(N) for dict
         # another method is to use list of [0,26] as A-Z since it's fixed char
 
-        nonRepeat = {}
-        maxLen = 0
-        startIdx = 0
-        for i in range(len(s)):
-            if s[i] not in nonRepeat:
-                nonRepeat[s[i]] = i
-            else:
-                nowLen = i-startIdx
-                maxLen = max(maxLen, nowLen)
-                oriIdx = nonRepeat[s[i]]
-                for clearIdx in range(startIdx, oriIdx+1, 1):
-                    del nonRepeat[s[clearIdx]]
-                startIdx = oriIdx+1
-                nonRepeat[s[i]] = i
-            # print('i: startIdx:', i, startIdx)
-            # print(nonRepeat)
-        # print(nonRepeat)
-        nowLen = len(s)-startIdx
-        maxLen = max(maxLen, nowLen)
-        return maxLen
+        # nonRepeat = {}
+        # maxLen = 0
+        # startIdx = 0
+        # for i in range(len(s)):
+        #     if s[i] not in nonRepeat:
+        #         nonRepeat[s[i]] = i
+        #     else:
+        #         nowLen = i-startIdx
+        #         maxLen = max(maxLen, nowLen)
+        #         oriIdx = nonRepeat[s[i]]
+        #         for clearIdx in range(startIdx, oriIdx+1, 1):
+        #             del nonRepeat[s[clearIdx]]
+        #         startIdx = oriIdx+1
+        #         nonRepeat[s[i]] = i
+        #     # print('i: startIdx:', i, startIdx)
+        #     # print(nonRepeat)
+        # # print(nonRepeat)
+        # nowLen = len(s)-startIdx
+        # maxLen = max(maxLen, nowLen)
+        # return maxLen
         
