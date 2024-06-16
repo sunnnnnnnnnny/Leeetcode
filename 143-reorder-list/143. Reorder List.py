9fc7122ca24c574d563fba5eed0505d97c30bb07
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-16 15:05:27 -0700

    Runtime: 52 ms (74.84%), Memory: 24.5 MB (94.97%) - Gitcode

--- 143-reorder-list/143. Reorder List.py
+++ 143-reorder-list/143. Reorder List.py
@@ -8,28 +8,60 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        # using the fast and slow pointer, then with slow one revert the par-child
+        # and connect with the slow point moving foward link the points
+        # time: O(N) spance:O(1)
+        fast = slow = head
+        preParent = None
+        while fast:
+            if slow.next != None:
+                preParent = slow
+                slow = slow.next
+            if fast.next != None and fast.next.next!=None:
+                fast = fast.next.next
+            else:
+                break
+        # to avoid having none with only one item
+        if preParent != None:
+            preParent.next = None
+        preParent = None
+        # reverse the second part
+        while slow:
+            tempSlow = slow.next
+            slow.next = preParent
+            preParent = slow
+            slow = tempSlow
+        # print(preParent)
+        first = head
+        while preParent != None:
+            tempFirst = first.next
+            tempSec = preParent.next
+            first.next = preParent
+            preParent.next = tempFirst
+            first = tempFirst
+            preParent = tempSec
         # brute force traverse the list and put in list, 
         # directly using their idx for reorder
         # time:O(2N) space:O(N) for new list
-        nodeList = []
-        cur = head
-        while cur:
-            nodeList.append(cur)
-            cur = cur.next
-        left = 0
-        right = len(nodeList)-1
-        newParent = ListNode()
-        parent = newParent
-        while left<=right:
-            parent.next = nodeList[left]
-            parent = parent.next
-            if left<right:
-                parent.next = nodeList[right]
-                parent = parent.next
-            parent.next = None
-            left +=1
-            right -=1
-        return newParent.next
+        # nodeList = []
+        # cur = head
+        # while cur:
+        #     nodeList.append(cur)
+        #     cur = cur.next
+        # left = 0
+        # right = len(nodeList)-1
+        # newParent = ListNode()
+        # parent = newParent
+        # while left<=right:
+        #     parent.next = nodeList[left]
+        #     parent = parent.next
+        #     if left<right:
+        #         parent.next = nodeList[right]
+        #         parent = parent.next
+        #     parent.next = None
+        #     left +=1
+        #     right -=1
+        # return newParent.next
 
 
 
