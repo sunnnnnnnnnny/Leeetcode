b99ffda5c950cdbd932b030d3f8e491ec34f5994
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-19 11:41:12 -0700

    Runtime: 108 ms (41.91%), Memory: 16.4 MB (99.64%) - Gitcode

--- 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
+++ 424-longest-repeating-character-replacement/424. Longest Repeating Character Replacement.py
@@ -1,27 +1,50 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        char2Freq = {}
+        start = end = 0
+        maxFreq = 0
+        maxLen = 0
+        while end<len(s):
+            if s[end] not in char2Freq.keys():
+                char2Freq[s[end]] = 0
+            char2Freq[s[end]] += 1
+            maxFreq = max(char2Freq[s[end]], maxFreq)
+            if end-start+1-maxFreq<=k:
+                maxLen = max(maxLen, end-start+1)
+                # print(start, end, maxFreq)
+            else:
+                while start<end:
+                    char2Freq[s[start]]-=1
+                    start += 1
+                    maxFreq = max(char2Freq.values())
+                    if end-start+1-maxFreq<=k:
+                        # print(start, end, char2Freq)
+                        break
+            end+=1
+        return maxLen
+
         # by not considering the fix letter, and only the window size
         # the valid str len is end-start+1-maxFreq<=k
         # we can move the start pointer to right when the window is not valid
         # by this approach we are only going through it once
         # time: O(N)
         # Space: O(M) for frequency storage
-        start = 0
-        freqMap = {}
-        maxFreq = 0
-        ret = 0
-        for end in range(len(s)):
-            freqMap[s[end]] = freqMap.get(s[end], 0)+1
-            # the only freq will change is the freqMap[s[end]]
-            # thus comparing the maxFreq is only necessary with the end letter
-            maxFreq = max(maxFreq, freqMap[s[end]])
+        # start = 0
+        # freqMap = {}
+        # maxFreq = 0
+        # ret = 0
+        # for end in range(len(s)):
+        #     freqMap[s[end]] = freqMap.get(s[end], 0)+1
+        #     # the only freq will change is the freqMap[s[end]]
+        #     # thus comparing the maxFreq is only necessary with the end letter
+        #     maxFreq = max(maxFreq, freqMap[s[end]])
 
-            isValid = (end-start+1-maxFreq<=k)
-            if not isValid:
-                freqMap[s[start]]-=1
-                start+=1
-            ret = end-start+1
-        return ret
+        #     isValid = (end-start+1-maxFreq<=k)
+        #     if not isValid:
+        #         freqMap[s[start]]-=1
+        #         start+=1
+        #     ret = end-start+1
+        # return ret
 
         # taking each single letter as fixed to search possible valid str
         # by sliding window of checking the substr
