8f3f377fc5375262c466280ffc53c77ac54be14a
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-19 15:44:55 -0700

    Runtime: 779 ms (24.96%), Memory: 27.3 MB (58.29%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
@@ -1,16 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # easier understanding is to find the min Price upto current point
+        minPrice = prices[0]
+        maxProfit = 0
+        for end in range(len(prices)):
+            maxProfit = max(maxProfit, prices[end]-minPrice)
+            minPrice = min(minPrice, prices[end])
+        return maxProfit
         # find the max diff from prev to current
         # shrisk the start if the no profit ending on day end
         # time: O(N) space:O(1)
-        start = 0
-        maxProfit = 0
-        curProfit = 0
-        for end in range(len(prices)):
-            curProfit = prices[end]-prices[start]
-            while curProfit<0:
-                start += 1
-                curProfit = prices[end]-prices[start]
-            maxProfit = max(maxProfit, curProfit)
-        return maxProfit
+        # start = 0
+        # maxProfit = 0
+        # curProfit = 0
+        # for end in range(len(prices)):
+        #     curProfit = prices[end]-prices[start]
+        #     while curProfit<0:
+        #         start += 1
+        #         curProfit = prices[end]-prices[start]
+        #     maxProfit = max(maxProfit, curProfit)
+        # return maxProfit
         
