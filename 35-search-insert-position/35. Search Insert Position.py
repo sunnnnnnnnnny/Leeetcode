3c890a4e95ab995fa184ba9a8869e665a3b0f86b
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-06-25 08:54:52 -0700

    Runtime: 45 ms (83.05%), Memory: 17.2 MB (68.03%) - Gitcode

---
+++ 35-search-insert-position/35. Search Insert Position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # binary search with left
+        left = 0
+        right = len(nums)-1
+        while left<=right:
+            mid = left+(right-left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid]>target:
+                right = mid-1
+            else:
+                left = mid+1
+        return left
+        
