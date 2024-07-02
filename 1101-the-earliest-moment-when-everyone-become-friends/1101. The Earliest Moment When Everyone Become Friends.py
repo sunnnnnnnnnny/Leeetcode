10d0bd87a1e82ccf2f45ab519b948d61fd3ef801
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-01 21:52:10 -0700

    Runtime: 102 ms (38.87%), Memory: 16.9 MB (34.06%) - Gitcode

--- 1101-the-earliest-moment-when-everyone-become-friends/1101. The Earliest Moment When Everyone Become Friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101. The Earliest Moment When Everyone Become Friends.py
@@ -1,8 +1,10 @@
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
         # similar to prerequset of classes
-        # union find with sort the timestamp by earliest
-        # time completxity;O(N*L) L is the log size
+        # union find O(N) with sort the timestamp by earliest O(MlogM)
+        # union through sorted logs =O(M*a(N))
+        # time completxity;O(N+MlogM+M*a(N)) M is the log size
+        # space:O(N+M) N for union group, M for sorting
         friendG = [i for i in range(n)]
         groupSize = [1 for i in range(n)]
         def findGroup(x):
@@ -14,11 +16,18 @@ class Solution:
         def union(x, y):
             xGroup = findGroup(x)
             yGroup = findGroup(y)
+            isMerged = False
+            if xGroup == yGroup:
+                return isMerged
+            isMerged = True
             if groupSize[xGroup]>=groupSize[yGroup]:
                 friendG[yGroup] = xGroup
             else:
                 friendG[xGroup] = yGroup
-        logs.sort()
+            return isMerged
+        logs.sort(key = lambda x:x[0])
+        groupCnt = n
+        # can be replaced by adding a counter at union stage
         def allAreFriends():
             zeroG = findGroup(0)
             for i in range(1, n):
@@ -28,8 +37,12 @@ class Solution:
             return True
         for log in logs:
             timeL = log[0]
-            union(log[1], log[2])
-            # check all frined group same
-            if allAreFriends():
+            isM = union(log[1], log[2])
+            if isM:
+                groupCnt -= 1
+            if groupCnt == 1:
                 return timeL
+            # check all frined group same
+            # if allAreFriends():
+            #     return timeL
         return -1
