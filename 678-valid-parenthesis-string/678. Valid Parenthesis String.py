0579e7b16bf5bc80a9a3bd7c244650f600ae5fc2
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-07 00:23:56 -0700

    Runtime: 33 ms (74.19%), Memory: 17.5 MB (6.91%) - Gitcode

--- 678-valid-parenthesis-string/678. Valid Parenthesis String.py
+++ 678-valid-parenthesis-string/678. Valid Parenthesis String.py
@@ -5,68 +5,61 @@ class Solution:
         # when end using them to balance it open>star
         # only open and star left, try balancing them by openLoc < starLoc
         # time: O(N) space:O(2N) => greedy
-        openIdx = []
-        starIdx = []
-        for i in range(len(s)):
-            if s[i] == "(":
-                openIdx.append(i)
-            elif s[i] == "*":
-                starIdx.append(i)
-            else:
-                if openIdx:
-                    openIdx.pop()
-                else:
-                    if starIdx:
-                        starIdx.pop()
-                    else:
-                        return False
-        while openIdx:
-            nowOpen = openIdx[-1]
-            if starIdx:
-                while starIdx:
-                    lastStarIdx = starIdx.pop()
-                    if lastStarIdx>nowOpen:
-                        openIdx.pop()
-                        break
-            else:
-                return False
-        return True
-        # backtracking dp[i][j] as if s[i:j] is working
-        # dp[i][j] == any(dp[i+1]dp[j-1], dp[i+2][j], dp[i][j-2])
-        # time: O(n*n) space:O(n*n)
-        # dp = [[0 for i in range(len(s))] for j in range(len(s))]
-        # def bk(start, end):
-        #     if start > end:
-        #         return 1
-        #     if start == end:
-        #         if s[start] == "*":
-        #             dp[start][end] = 1
+        # openIdx = []
+        # starIdx = []
+        # for i in range(len(s)):
+        #     if s[i] == "(":
+        #         openIdx.append(i)
+        #     elif s[i] == "*":
+        #         starIdx.append(i)
+        #     else:
+        #         if openIdx:
+        #             openIdx.pop()
         #         else:
-        #             dp[start][end] = -1
-        #         return dp[start][end]
-        #     if dp[start][end] != 0:
-        #         return dp[start][end]
-        #     # the right and left
-        #     ret = -1
-        #     if s[start] == "*":
-        #         if s[end] == ")":
-        #             if bk(start+1, end-1)==1:
-        #                 ret = 1
-        #         if bk(start+1, end)==1:
-        #                 ret = 1
-            
-        #     if s[start] == "(":
-        #         if s[end] == ")" or s[end] == "*":
-        #             if bk(start+1, end-1)==1:
-        #                 ret = 1
-        #         if s[end] == "*":
-        #             if bk(start, end-1)==1:
-        #                     ret = 1
-        #     dp[start][end] = ret
-        #     return ret
-        # ans = bk(0,len(s)-1)
-        # return ans
-            
+        #             if starIdx:
+        #                 starIdx.pop()
+        #             else:
+        #                 return False
+        # while openIdx:
+        #     nowOpen = openIdx[-1]
+        #     if starIdx:
+        #         while starIdx:
+        #             lastStarIdx = starIdx.pop()
+        #             if lastStarIdx>nowOpen:
+        #                 openIdx.pop()
+        #                 break
+        #     else:
+        #         return False
+        # return True
+        # backtracking dp[i][j] represents 
+        # if the substring starting from index i is valid with j opening brackets
+        # dp[i][j] == any(dp[i+1][j-1], dp[i+1][j], dp[i+1][j+1])
+        # time: O(n*n) space:O(n*n)
+        n = len(s)
+        dp = [[-1] *(n) for _ in range(n)]
+        def bk(idx, openCnt):
+            if idx == len(s):
+                return openCnt == 0
+            if dp[idx][openCnt]!=-1:
+                return dp[idx][openCnt] == -1
+            isValid = False
+            if s[idx] == "*":
+                # treat as open
+                isValid |= bk(idx+1, openCnt+1)
+                if openCnt>0:
+                    # treat as close
+                    isValid |= bk(idx+1, openCnt-1)
+                isValid |= bk(idx+1, openCnt)
+            else:
+                if s[idx] == "(":
+                    isValid |= bk(idx+1, openCnt+1)
+                elif openCnt>0:
+                    isValid |= bk(idx+1, openCnt-1)
+            dp[idx][openCnt] = 1 if isValid else 0
+            return isValid
+        
+        return bk(0, 0)
+
 
 
             
