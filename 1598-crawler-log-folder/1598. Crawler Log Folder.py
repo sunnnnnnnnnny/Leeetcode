09c09cf3c52323fcd0bf199722127f7aff08911d
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-09 23:29:03 -0700

    Runtime: 49 ms (58.45%), Memory: 16.9 MB (11.07%) - Gitcode

--- 1598-crawler-log-folder/1598. Crawler Log Folder.py
+++ 1598-crawler-log-folder/1598. Crawler Log Folder.py
@@ -1,16 +1,26 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
         # get the level distance from the main folder after operation
-        # using queue to store the current folder
-        # time:O(N) space:O(N) queue size is at most len of logs
-        folderQ = []
+        # using stack to store the current folder
+        # time:O(N) space:O(N) stack size is at most len of logs
+        # or using counter will save the space
+        # folderQ = []
+        # for log in logs:
+        #     if log == "../":
+        #         if len(folderQ)>0:
+        #             folderQ.pop()
+        #     else:
+        #         if log != "./":
+        #             folderQ.append(log)
+        # return len(folderQ)
+        levelCnt = 0
         for log in logs:
             if log == "../":
-                if len(folderQ)>0:
-                    folderQ.pop()
+                if levelCnt>0:
+                    levelCnt -= 1
             else:
                 if log != "./":
-                    folderQ.append(log)
-        return len(folderQ)
+                    levelCnt += 1
+        return levelCnt
 
         
