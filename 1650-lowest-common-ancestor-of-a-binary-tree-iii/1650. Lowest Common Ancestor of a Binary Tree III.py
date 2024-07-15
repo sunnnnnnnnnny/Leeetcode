89ee4f30c2886a45575c3c2cb07bcc73ec09a3b4
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-15 09:41:29 -0700

    Runtime: 47 ms (71.53%), Memory: 20.6 MB (9.70%) - Gitcode

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650. Lowest Common Ancestor of a Binary Tree III.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650. Lowest Common Ancestor of a Binary Tree III.py
@@ -10,19 +10,42 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        # get both len of p and q to root
+        # move the lower one
+        # allowing p and q start on the same height to seek LCA
+        # time:O((height(p)+height(q))*2)
+        # space:O(1)
+        def findHeight(now):
+            if now == None:
+                return 0
+            return findHeight(now.parent)+1
+        pHeight = findHeight(p)
+        qHeight = findHeight(q)
+        pPar = p
+        qPar = q
+        if pHeight>qHeight:
+            for _ in range(pHeight-qHeight):
+                pPar = pPar.parent
+        elif pHeight<qHeight:
+            for _ in range(qHeight-pHeight):
+                qPar = qPar.parent
+        while pPar != qPar:
+            pPar = pPar.parent
+            qPar = qPar.parent
+        return pPar
         # both p and q going to the root
         # the first node seeing p and q is the LCA
         # time: O(p to root len + q to root len)
         # space: same as time
-        pParent = set()
-        cur = p
-        while cur!=None:
-            pParent.add(cur)
-            cur = cur.parent
-        cur = q
-        while cur!=None:
-            if cur in pParent:
-                return cur
-            cur = cur.parent
-        return None
+        # pParent = set()
+        # cur = p
+        # while cur!=None:
+        #     pParent.add(cur)
+        #     cur = cur.parent
+        # cur = q
+        # while cur!=None:
+        #     if cur in pParent:
+        #         return cur
+        #     cur = cur.parent
+        # return None
         
