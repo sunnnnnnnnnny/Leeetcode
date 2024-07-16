b9ca9d46f7e65516d389507e809dfede3a4be92c
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-15 20:46:18 -0700

    Runtime: 38 ms (39.56%), Memory: 16.8 MB (20.28%) - Gitcode

---
+++ 339-nested-list-weight-sum/339. Nested List Weight Sum.py
@@ -0,0 +1,61 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        # assume it's always a valid list NestedInteger
+        # by keep record of current depth, we can go through the whole list for ret
+        # time:O(N) space:O(1)
+        ret = 0
+        def getDepMul(now:NestedInteger, depth:int):
+            nonlocal ret
+            if now.isInteger():
+                ret += now.getInteger()*depth
+                return
+            listNest = now.getList()
+            for nestItem in listNest:
+                getDepMul(nestItem, depth+1)
+        for nestItemLev1 in nestedList:
+                getDepMul(nestItemLev1, 1)
+        return ret
+        
