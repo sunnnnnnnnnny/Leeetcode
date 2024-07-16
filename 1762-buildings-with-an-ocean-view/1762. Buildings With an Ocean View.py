1140d19fe62863811ab85694f633bc129c23fa95
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-15 22:47:40 -0700

    Runtime: 6929 ms (5.05%), Memory: 34.2 MB (13.04%) - Gitcode

---
+++ 1762-buildings-with-an-ocean-view/1762. Buildings With an Ocean View.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        # get the highest building height from the right
+        # keep such record and add the index of building
+        # if the building height>highest height of right
+        # assume all height is positive
+        # time:O(N) space:O(1)
+        hightestR = 0
+        ret = []
+        for i in range(len(heights)-1, -1, -1):
+            if heights[i]>hightestR:
+                ret = [i]+ret
+            hightestR = max(hightestR, heights[i])
+        return ret
