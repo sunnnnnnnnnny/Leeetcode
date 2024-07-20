b08666fc772ff85645061f1b5c78d3abe717a552
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-19 22:24:41 -0700

    Runtime: 2090 ms (11.63%), Memory: 73.7 MB (70.38%) - Gitcode

--- 1868-product-of-two-run-length-encoded-arrays/1868. Product of Two Run-Length Encoded Arrays.py
+++ 1868-product-of-two-run-length-encoded-arrays/1868. Product of Two Run-Length Encoded Arrays.py
@@ -2,6 +2,8 @@ class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
         # by going through both array we can get the procudt
         # record the num 2 freq direct and convert back to array
+        # two pointers
+        # time: O(N) space:O(N) for result
         oIdx = tIdx = 0
         oinidx = tinidx = 0
         ret = []
@@ -10,22 +12,18 @@ class Solution:
             oFreq = encoded1[oIdx][1]
             tNum = encoded2[tIdx][0]
             tFreq = encoded2[tIdx][1]
-            while oinidx<oFreq and tinidx<tFreq:
-                oLeft = oFreq-oinidx
-                tLeft = tFreq-tinidx
-                prodSetCnt = min(oLeft, tLeft)
-                prod = oNum*tNum
-                if len(ret)>0 and prod==ret[-1][0]:
-                    ret[-1][1] += prodSetCnt
-                else:
-                    ret.append([prod, prodSetCnt])
-                oinidx += prodSetCnt
-                tinidx += prodSetCnt
-            if oinidx>=oFreq:
-                oinidx = 0
+
+            prodSetCnt = min(oFreq, tFreq)
+            prod = oNum*tNum
+            if len(ret)>0 and prod==ret[-1][0]:
+                ret[-1][1] += prodSetCnt
+            else:
+                ret.append([prod, prodSetCnt])
+            encoded1[oIdx][1] -= prodSetCnt
+            encoded2[tIdx][1] -= prodSetCnt
+            if encoded1[oIdx][1] == 0:
                 oIdx += 1
-            if tinidx>=tFreq:
-                tinidx = 0
+            if encoded2[tIdx][1] == 0:
                 tIdx += 1
         
         return ret
