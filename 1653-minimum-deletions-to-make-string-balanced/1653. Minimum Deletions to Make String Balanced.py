1c98f0c86c00ea496f6db0916d1818314322b1f4
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-07-30 09:59:06 -0700

    Runtime: 478 ms (37.34%), Memory: 17.6 MB (92.33%) - Gitcode

--- 1653-minimum-deletions-to-make-string-balanced/1653. Minimum Deletions to Make String Balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653. Minimum Deletions to Make String Balanced.py
@@ -4,20 +4,35 @@ class Solution:
         # find each indxe, of a before it and b after it
         # the min delete is by getting the max of sum im step 2
         # time:O(N) space(2N)
-        aCount = [0]*len(s)
-        bCount = [0]*len(s)
-        for i in range(0,len(s), 1):
-            reverseI = len(s)-1-i
-            aCount[i] = aCount[i-1] if i>0 else 0
-            bCount[reverseI] = bCount[reverseI+1] if i>0 else 0
-            if s[i] == 'a':
-                aCount[i] +=1 
-            if s[reverseI] == 'b':
-                bCount[reverseI] +=1
-        maxAB = 0
-        # print(aCount)
-        # print(bCount)
-        for i in range(0,len(s), 1):
-            nowAB = aCount[i]+bCount[i]
-            maxAB = max(maxAB, nowAB)
-        return len(s)-maxAB
+        # second way is one pass as we don't delete the a on left but b on right
+        # by delete the b on left and a on right will be the min deletion
+        # time :O(N) space: O(1)
+        n = len(s)
+        aTotal = sum(1 for ch in s if ch == 'a')
+        aRight = aTotal
+        bLeft = 0
+        minDelete = n
+        for ch in s:
+            if ch == 'a':
+                aRight -= 1
+            minDelete = min(minDelete, aRight+bLeft)
+            if ch == 'b':
+                bLeft += 1
+        return minDelete
+        # aCount = [0]*len(s)
+        # bCount = [0]*len(s)
+        # for i in range(0,len(s), 1):
+        #     reverseI = len(s)-1-i
+        #     aCount[i] = aCount[i-1] if i>0 else 0
+        #     bCount[reverseI] = bCount[reverseI+1] if i>0 else 0
+        #     if s[i] == 'a':
+        #         aCount[i] +=1 
+        #     if s[reverseI] == 'b':
+        #         bCount[reverseI] +=1
+        # maxAB = 0
+        # # print(aCount)
+        # # print(bCount)
+        # for i in range(0,len(s), 1):
+        #     nowAB = aCount[i]+bCount[i]
+        #     maxAB = max(maxAB, nowAB)
+        # return len(s)-maxAB
