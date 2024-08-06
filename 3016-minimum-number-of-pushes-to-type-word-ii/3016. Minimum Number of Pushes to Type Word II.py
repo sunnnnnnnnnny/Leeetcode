83d84404fc17832dac4d4500f737fa45357fb9c5
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-08-05 23:06:02 -0700

    Runtime: 267 ms (8.59%), Memory: 17.7 MB (36.75%) - Gitcode

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016. Minimum Number of Pushes to Type Word II.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016. Minimum Number of Pushes to Type Word II.py
@@ -5,20 +5,23 @@ class Solution:
         # time: O(N+26log26+26) = O(N) for sorting
         # space:O(1) cause we only need 8 key to record the mapping
         maxKey = 8
-        keyCnt = [1]*maxKey
+        # keyCnt = [1]*maxKey
         char2Freq = [0]*26
         for i in range(len(word)):
             letter = ord(word[i])-ord('a')
             char2Freq[letter]+=1
         char2Freq.sort(reverse=True)
-        keyIdx = 0
+        # keyIdx = 0
         ret = 0
+        idx = 0
         for freq in char2Freq:
             if freq == 0:
                 break
-            ret += (keyCnt[keyIdx]*freq)
-            keyCnt[keyIdx]+=1
-            keyIdx+=1
-            if keyIdx>=maxKey:
-                keyIdx = 0
+            ret += (freq*(idx//maxKey+1))
+            idx += 1
+            # ret += (keyCnt[keyIdx]*freq)
+            # keyCnt[keyIdx]+=1
+            # keyIdx+=1
+            # if keyIdx>=maxKey:
+            #     keyIdx = 0
         return ret
