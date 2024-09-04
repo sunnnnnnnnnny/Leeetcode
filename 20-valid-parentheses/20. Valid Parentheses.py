f5c18812960cc435752acbcb3b4b5b4a5787221e
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-04 09:18:45 -0700

    Runtime: 33 ms (73.21%), Memory: 16.4 MB (90.74%) - Gitcode

--- 20-valid-parentheses/20. Valid Parentheses.py
+++ 20-valid-parentheses/20. Valid Parentheses.py
@@ -1,38 +1,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # using FILO queue to record the open bracket
-        # once meeting the close bracket, pop the last open one
-        # check if the type is correct, and at the end if open queue>0
-        # TIme; O(N) traverse the str with each pop cost O(1)
-        # Space:O(N) max all open bracket goes into the queue
-        # or convert the open bracket as close brackt str, 
-        # and move the pointers when meeting one close, to verify the validtion
-        # Time: O(N) Space:O(N)
-        open = deque()
-        # def getCloseBracket(char):
-        #     if char == '(' :
-        #         return ')'
-        #     elif char =='{' :
-        #         return '}'
-        #     elif char == '[':
-        #         return ']'
-        #     return ''
-        bracketMap = {
-            '{':'}',
-            '(':')',
-            '[':']'
-        }
-        for char in s:
-            if char in ('(' , '{', '['):
-                open.append(char)
-            elif char in (')', '}', ']'):
-                if len(open)==0:
-                    return False
-                lastOpen = open.pop()
-                # shouldClose = getCloseBracket(lastOpen)
-                if bracketMap[lastOpen] != char:
-                    return False
+        # push all the open into queue, everything meets a close pop one
+        # and check if it match the set, time:O(n) space:O(n)
+        # assume no other type of input included
+        openB = []
+        def getMatch(openOne):
+            if openOne == '(':
+                return ')'
+            elif openOne == '[':
+                return ']'
+            return '}'
+        for i in range(len(s)):
+            if s[i] in ('(', '[', '{'):
+                matchClose = getMatch(s[i])
+                openB.append(matchClose)
             else:
-                return False
-        return len(open) == 0
-        
+                # FILO is pop(), FIFO is pop(0)
+                if len(openB)==0:
+                    return False
+                latestClose = openB.pop()
+                if latestClose != s[i]:
+                    return False
+        return True if len(openB)==0 else False
