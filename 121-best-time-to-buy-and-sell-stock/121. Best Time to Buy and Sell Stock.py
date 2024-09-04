a980f8d80351f0efba0f5f01890c267cb4408288
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-03 23:12:57 -0700

    Runtime: 785 ms (19.18%), Memory: 27.4 MB (91.49%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
@@ -1,18 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # want to find the min of left max of right
-        # using prefix array to record the min n from left, 
-        # and max n from the right. 
-        # thus at each point we get the max gain at each location
-        # time: O(2n) space:O(n)
-        lMin = [0]*len(prices)
-        lMin[0] = prices[0]
-        for i in range(1, len(prices)):
-            lMin[i] = min(lMin[i-1], prices[i])
-        maxR = prices[-1]
+        # could record the min up to now, then get the maxGain
+        # time:O(n) space:O(1)
+        lMin = prices[0]
         maxGain = 0
-        for i in range(len(prices)-1, -1,-1):
-            maxR = max(maxR, prices[i])
-            maxGain = max(maxGain, maxR-lMin[i])
+        for p in prices:
+            lMin = min(lMin, p)
+            maxGain = max(maxGain, p-lMin)
+        # # want to find the min of left max of right
+        # # using prefix array to record the min n from left, 
+        # # and max n from the right. 
+        # # thus at each point we get the max gain at each location
+        # # time: O(2n) space:O(n)
+        # lMin = [0]*len(prices)
+        # lMin[0] = prices[0]
+        # for i in range(1, len(prices)):
+        #     lMin[i] = min(lMin[i-1], prices[i])
+        # maxR = prices[-1]
+        # maxGain = 0
+        # for i in range(len(prices)-1, -1,-1):
+        #     maxR = max(maxR, prices[i])
+        #     maxGain = max(maxGain, maxR-lMin[i])
         
         return maxGain
