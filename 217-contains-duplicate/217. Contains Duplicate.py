b528c1611142df9cf439e9bc41a0b5934c4f52fa
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-03 22:23:58 -0700

    Runtime: 399 ms (98.00%), Memory: 34.5 MB (16.03%) - Gitcode

--- 217-contains-duplicate/217. Contains Duplicate.py
+++ 217-contains-duplicate/217. Contains Duplicate.py
@@ -1,11 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # hash table to record exist, then check it -> time:O(n) space:O(n)
-        # sort the array and go through it-> time:O(nlogn) space;O(1)
-        exist = {}
-        for n in nums:
-            if n in exist.keys():
+        # hashmap to record the number appeared
+        # return true when there's same num appeared
+        # time: O(N) traverse the nums and look up in hash O(1)
+        # space: O(N) for hashmap
+        hashMap = {}
+        for num in nums:
+            if num in hashMap.keys():
                 return True
-            exist[n] = True
+            hashMap[num] = 1
         return False
         
