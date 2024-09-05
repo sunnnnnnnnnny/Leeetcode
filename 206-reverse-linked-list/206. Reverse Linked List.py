5064891f920120a143cdb43eb228e3000f409733
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-05 10:05:49 -0700

    Runtime: 41 ms (29.03%), Memory: 17.7 MB (28.85%) - Gitcode

--- 206-reverse-linked-list/206. Reverse Linked List.py
+++ 206-reverse-linked-list/206. Reverse Linked List.py
@@ -5,19 +5,39 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # using recursive to get the next node and revert it
-        # also using gobal variable to record the end as the new head
-        # time: O(N) traverse
-        # space: O(N) recrusive taking all the nodes
-        self.oriEndAsNewHead = ListNode(0, None)
-        def getOriChild(self, now, parent):
-            if not now:
-                self.oriEndAsNewHead.next = parent
-                return
-            getOriChild(self, now.next, now)
-            now.next = parent
-            return
-        getOriChild(self, head, None)
-        return self.oriEndAsNewHead.next
+        # dp recursive to make it, while loop with var
+        # time:O(N) space:O(n) for recursive O(1) for while
+        if head is None:
+            return head
+        prev = None
+        cur = head
+        while cur!=None:
+            nextCur = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nextCur
+        return prev
+
+
+
+
+
+
+
+
+        # # using recursive to get the next node and revert it
+        # # also using gobal variable to record the end as the new head
+        # # time: O(N) traverse
+        # # space: O(N) recrusive taking all the nodes
+        # self.oriEndAsNewHead = ListNode(0, None)
+        # def getOriChild(self, now, parent):
+        #     if not now:
+        #         self.oriEndAsNewHead.next = parent
+        #         return
+        #     getOriChild(self, now.next, now)
+        #     now.next = parent
+        #     return
+        # getOriChild(self, head, None)
+        # return self.oriEndAsNewHead.next
 
         
