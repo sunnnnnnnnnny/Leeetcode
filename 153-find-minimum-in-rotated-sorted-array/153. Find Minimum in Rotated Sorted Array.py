5e0b6093e35b2e1f95ba39a6726489cf4bb2faef
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-05 22:06:16 -0700

    Runtime: 50 ms (10.60%), Memory: 16.9 MB (7.48%) - Gitcode

--- 153-find-minimum-in-rotated-sorted-array/153. Find Minimum in Rotated Sorted Array.py
+++ 153-find-minimum-in-rotated-sorted-array/153. Find Minimum in Rotated Sorted Array.py
@@ -1,23 +1,26 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        # bruteforce traverse all the list O(N)
-        # binary search the array with left binary search
+        # brute force is directly get the rotation point O(N)
+        # binary search can get the the min in O(logN)
+        # the min would appear in 3 type
+        # [0,1,2,3,4], in left=>if right side is sorted and mid>mid-1
+        # [4,0,1,2,3] in left=>if right side is sorted and mid>mid-1
+        # [2,3,4,0,1] in right=>if left side is sorted and left>right
+        theMin = nums[0]
         left = 0
         right = len(nums)-1
-        if len(nums) == 1:
-            return nums[0]
-        if nums[0]<nums[right]:
-            return nums[0]
-        while left<=right:
-            mid = left+(right-left)//2
-            if nums[mid]>nums[mid+1]:
-                return nums[mid+1]
-            if nums[mid-1]>nums[mid]:
-                return nums[mid]
-            if nums[mid]>nums[0]:
-                left = mid+1
+        while left<right:
+            mid = (left+right)//2
+            theMin = min(theMin, nums[mid])
+            if nums[right]>nums[mid]:
+                if mid>0 and nums[mid]>nums[mid-1]:
+                    right = mid-1
+                else:
+                    left = mid+1
             else:
-                right = mid-1
-
-
-        
+                if nums[left]>nums[right]:
+                    left = mid+1
+                else:
+                    right = mid-1
+        theMin = min(theMin, nums[left])
+        return theMin
