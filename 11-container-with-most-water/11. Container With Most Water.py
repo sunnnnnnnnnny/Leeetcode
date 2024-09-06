e1d1f68cb512b7fc3f327d06b825f6db5276e95a
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-05 19:55:02 -0700

    Runtime: 527 ms (45.78%), Memory: 29.1 MB (91.91%) - Gitcode

--- 11-container-with-most-water/11. Container With Most Water.py
+++ 11-container-with-most-water/11. Container With Most Water.py
@@ -1,17 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # start from both sides, move the shorter edge to higher one
-        # in order to increase the container size
-        # time: O(N) space:O(1)
+        # need to find the height on 2 ends
+        # 2pointer to move from the two ends
+        # move the shorter edge b/c it only have a chance be larger area while having higher end
+        # assume hieght>0
         left = 0
         right = len(height)-1
-        maxSize = min(height[left], height[right])*(right-left)
+        maxArea = min(height[left], height[right])*(right-left)
         while left<right:
-            curSize = min(height[left], height[right])*(right-left)
-            maxSize = max(curSize, maxSize)
-            if height[left]< height[right]:
+            nowArea = min(height[left], height[right])*(right-left)
+            maxArea = max(maxArea, nowArea)
+            # print(nowArea, left, right)
+            if height[left]<height[right]:
                 left+=1
             else:
-                right -=1
-        return maxSize
-        
+                right-=1
+        return maxArea
