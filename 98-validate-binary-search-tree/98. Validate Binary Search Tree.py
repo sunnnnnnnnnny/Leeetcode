4dc10db287b47d84d00a35eb459a3df818b1f866
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-07 17:04:59 -0700

    Runtime: 45 ms (31.36%), Memory: 18.3 MB (90.20%) - Gitcode

--- 98-validate-binary-search-tree/98. Validate Binary Search Tree.py
+++ 98-validate-binary-search-tree/98. Validate Binary Search Tree.py
@@ -6,19 +6,36 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # inorder traversal should be increasing order
+        # time:O(N) space:O(N)
+        def checkInorder(now, prevN):
+            if now == None:
+                return True, prevN
+            
+            valid, nextPrev = checkInorder(now.left, prevN)
+            if not valid:
+                return False, nextPrev
+
+            if now.val <= nextPrev:
+                return False, now.val
+            
+            valid, nextPrev = checkInorder(now.right, now.val)
+            return valid, nextPrev
+        validCheck, prev = checkInorder(root, -inf)
+        return validCheck
         # dfs with checking the min and max num 
         # time:O(N) space:O(N)
-        def checkValid(now, minN, maxN):
-            if now == None:
-                return True
-            # print(now.val, minN, maxN)
-            if now.val<=minN or now.val>=maxN:
-                return False
-            # check left tree
-            if not checkValid(now.left, minN, min(maxN, now.val)):
-                return False
-            # check right
-            if not checkValid(now.right, max(minN, now.val), maxN):
-                return False
-            return True
-        return checkValid(root, -inf, inf)
+        # def checkValid(now, minN, maxN):
+        #     if now == None:
+        #         return True
+        #     # print(now.val, minN, maxN)
+        #     if now.val<=minN or now.val>=maxN:
+        #         return False
+        #     # check left tree
+        #     if not checkValid(now.left, minN, min(maxN, now.val)):
+        #         return False
+        #     # check right
+        #     if not checkValid(now.right, max(minN, now.val), maxN):
+        #         return False
+        #     return True
+        # return checkValid(root, -inf, inf)
