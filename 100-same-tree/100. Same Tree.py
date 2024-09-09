710d3cc9dd23a3c3c1433bb890172a3bc7d31417
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-08 23:50:45 -0700

    Runtime: 38 ms (34.90%), Memory: 16.5 MB (37.77%) - Gitcode

--- 100-same-tree/100. Same Tree.py
+++ 100-same-tree/100. Same Tree.py
@@ -6,46 +6,17 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        # output the tree as pre-order traversal, with empty node as null string
-        # then compare the string, if not then no
-        def preOrder(root):
-            ret = ""
-            bfs = [root]
-            while bfs:
-                now = bfs.pop(0)
-                if now is None:
-                    ret += " null"
-                else:
-                    ret += " "
-                    ret+=str(now.val)
-                    bfs.append(now.left)
-                    bfs.append(now.right)
-            return ret
-        pStr = preOrder(p)
-        qStr = preOrder(q)
+        # print as preorder traversal with none child as #
+        # then make string compare
+        # N is tree p size, M is tree q size
+        # time:O(N+M+ max(N,M)) the max(N,M) is the string compare time
+        # space:O(N+M)
+        def preOrderStr(now, nowStr):
+            if now == None:
+                return nowStr+"#"
+            nextStr = nowStr+str(now.val)
+            nextStr = preOrderStr(now.left, nextStr)
+            return preOrderStr(now.right, nextStr)
+        pStr = preOrderStr(p, "")
+        qStr = preOrderStr(q, "")
         return pStr == qStr
-        # traverse the tree in same order and compare
-        # yet how to confirm if they are at the same level?
-        # adding level form to confirm
-        # ex. dfs 0_1 1_2 1_3
-        # ex. bfs 1 l 2 3 l
-        # time: O(N) space: O(N)
-        def getBfsOrder(root: Optional[TreeNode]):
-            retStr = ""
-            order = deque([root])
-            while order:
-                levelCnt = len(order)
-                for i in range(levelCnt):
-                    now = order.popleft()
-                    if now == None:
-                        retStr += " null"
-                        continue
-                    retStr += str(now.val)
-                    retStr += " "
-                    order.append(now.left)
-                    order.append(now.right)
-                retStr+="l "
-            return retStr
-        qInStr = getBfsOrder(q)
-        pInStr = getBfsOrder(p)
-        return qInStr == pInStr
