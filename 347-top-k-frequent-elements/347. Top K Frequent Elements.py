5e477785585e598945eade479c7011bed33270fc
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-08 20:41:53 -0700

    Runtime: 78 ms (95.69%), Memory: 21.2 MB (69.81%) - Gitcode

--- 347-top-k-frequent-elements/347. Top K Frequent Elements.py
+++ 347-top-k-frequent-elements/347. Top K Frequent Elements.py
@@ -1,33 +1,26 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # go through the list one by one with keep record of the top K freq items
-        # by having the  top k freq cnt, if any item exceed that then we exchange
-        # with adding it to the cnt then remove the least freq in top k list
-        # time:O(N+MlogK) M is the unique nums as we do at most M pop
-        # M is <= N
-        # space:O(3N)  = O(N)
-        numToFreq = collections.Counter(nums)
-        freqToNum = {}
-        topKQueue = []
-        for num, freq in numToFreq.items():
-            if freq not in freqToNum.keys():
-                freqToNum[freq] = []
-            freqToNum[freq].append(num)
-            if len(topKQueue)<k:
-                heapq.heappush(topKQueue, freq)
+        # sort the nums, then by getting the freq also updating the top k freq list (minHeap)
+        # time: O(nlogn+nlogK) space:O(N)
+        # gothrough the list and count the freq, with minHeap of size k, pop the higher Freq into heap
+        # time:O(N*logK+K) space:O(N)
+        freq = {}
+        for n in nums:
+            if n not in freq.keys():
+                freq[n] = 0
+            freq[n]+=1
+
+        kTopFreq = []
+        for key, val in freq.items():
+            if len(kTopFreq)<k:
+                heapq.heappush(kTopFreq, (val, key))
             else:
-                if topKQueue[0]<freq:
-                    heapq.heappop(topKQueue)
-                    heapq.heappush(topKQueue, freq)
-        ans = []
-        while len(topKQueue)>0:
-            freq = heapq.heappop(topKQueue)
-            if freq in freqToNum.keys():
-                ans += freqToNum[freq]
-                del freqToNum[freq]
-        return ans
-
-
-
-
-        
+                if val>kTopFreq[0][0]:
+                    heapq.heappop(kTopFreq)
+                    heapq.heappush(kTopFreq, (val, key))
+                
+        ret = []
+        while len(kTopFreq)>0:
+            heapMinHeadFreq, heapMinNum = heapq.heappop(kTopFreq)
+            ret.append(heapMinNum)
+        return ret
