c788a108c916c0f70299cab877dadd45632c1a29
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-09 23:07:34 -0700

    Runtime: 59 ms (94.81%), Memory: 16.7 MB (53.51%) - Gitcode

--- 572-subtree-of-another-tree/572. Subtree of Another Tree.py
+++ 572-subtree-of-another-tree/572. Subtree of Another Tree.py
@@ -6,31 +6,46 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # by comparing the root and subroot, we need to find the subRoot
-        # only if both node exist in same direct will it be subtree
-        ret = False
-        def compare2Tree(now, sub):
-            if now == None and sub == None:
-                return True
-            if now == None or sub == None:
-                return False
-            if  now.val != sub.val:
-                return False
-            return compare2Tree(now.left, sub.left) and compare2Tree(now.right, sub.right) 
+        # bydoing preorder or postorder we could see if the subroot is in the tree
+        # also in order to have the correct child, we need to print delimieter for it
+        # time: O(N+M+N) traversal both tree takes, N and M
+        # the time to find the substring is O(N)
+        # spaceL:O(N+M) as we need the postOrder of both trees
+        def postOrder(now):
+            if now == None:
+                return "# "
+            left = postOrder(now.left)
+            right = postOrder(now.right)
+            return left+right+str(now.val)+" "
+        rootPostOrder = postOrder(root)
+        subPostOrder = postOrder(subRoot)
+        # print(rootPostOrder)
+        # print(subPostOrder)
+        # print(rootPostOrder.find(subPostOrder))
+        return rootPostOrder.find(subPostOrder)>=0
+        # do the traverse of root then compare each node to subroot
+        # brute force: time= O(M*N) space:O(M+N)
+        # serialize both trees, then check if subtree is the substring
+        # by adding null as empty indicator for the preorder / postorder
+        # time: O(M+N) space: O(M+N)
+        # def preOrder(now:Optional[TreeNode]):
+        #     preStr = ""
+        #     trav = deque()
+        #     trav.append(now)
+        #     while trav:
+        #         levelCnt = len(trav)
+        #         for i in range(levelCnt):
+        #             cur = trav.popleft()
+        #             if cur == None:
+        #                 preStr += "Null "
+        #                 continue
+        #             trav.append(cur.left)
+        #             trav.append(cur.right)
+        #             preStr += str(cur.val)
+        #             preStr += " "
+        #     return preStr
+        # rootStr = preOrder(root)
+        # subStr = preOrder(subRoot)
+        # return rootStr.find(subStr)
             
-        def dfsCompare(now, subroot):
-            nonlocal ret
-            if now == None and subroot == None:
-                ret = True
-                return
-            if now == None or subroot == None:
-                return
-            
-            if now.val == subroot.val:
-                if compare2Tree(now, subroot):
-                    ret = True
-            dfsCompare(now.left, subroot)
-            dfsCompare(now.right, subroot)
-        dfsCompare(root, subRoot)
-        return ret
-            
+        
