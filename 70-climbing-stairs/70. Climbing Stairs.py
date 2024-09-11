59bcf96a8845d1fab17a824bdebca206cfea6319
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-11 09:38:18 -0700

    Runtime: 36 ms (45.40%), Memory: 16.4 MB (61.97%) - Gitcode

--- 70-climbing-stairs/70. Climbing Stairs.py
+++ 70-climbing-stairs/70. Climbing Stairs.py
@@ -1,21 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # dp being the n-1 + n-2 as both can reach stair n
-        # dpList = list()
-        # at step 0 is 1 while step 1 is 1
-        # dpList.append(1)
-        # dpList.append(1)
-        # for i in range(2, n+1, 1):
-        #     dpList.append(dpList[i-1]+dpList[i-2])
-        dp = [0]*(n+1)
-        dp[0] = 1
-        dp[1] = 1
-        if n < 2:
-            return dp[n]
-        for i in range(2, n+1, 1):
-            dp[i] = dp[i-2]+dp[i-1]
-        return dp[n]
-        
-
-
-        
+        # dp bottom up to calculate the steps
+        # dp[i] = dp[i-1]+dp[i-2]
+        # time:O(N) space:O(N)
+        # save space only use 2 var to record the i-1, i-2
+        # time:O(N) space:O(1)
+        # dp = [0 for _ in range(n+1)]
+        if n<1:
+            return 0
+        # dp[0] = dp[1] = 1
+        # for i in range(2, n+1):
+        #     dp[i] = dp[i-1]+dp[i-2]
+        dp0 = dp1 = 1
+        now = 0
+        for i in range(2, n+1):
+            now = dp0+dp1
+            dp0 = dp1
+            dp1 = now
+        return dp1
