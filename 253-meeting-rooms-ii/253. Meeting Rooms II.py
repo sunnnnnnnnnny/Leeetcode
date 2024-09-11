e1433191ce08f55a2fc95fad99839d7a71d77aa7
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-11 00:15:28 -0700

    Runtime: 61 ms (98.05%), Memory: 19.9 MB (91.84%) - Gitcode

--- 253-meeting-rooms-ii/253. Meeting Rooms II.py
+++ 253-meeting-rooms-ii/253. Meeting Rooms II.py
@@ -1,26 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # 1. sort+traverse with min heap
-        # sort by ending, then record each meeting room's starting time
-        # using min heap to keep record of the earliest possible released room
-        # assgin that empty room to the new meeting
-        # (wrong)traverse all interval by try to fit in existing room
-        # time: O(nlong+n) = O(nlogn)
-        # Space: O(N+N) = sort+traversal to record room time
+        # sort the time from start?
+        # record the occupied meeting room end time, with minHead
+        # use the first released meeting room
+        # time;O(NlogN+N) space:O(N)
         room = []
-        intervals.sort(key=lambda x:x[0])
-        # take the first meeting directly
-        room.append(intervals[0][1])
-        heapq.heapify(room)
- 
-        for start, end in intervals[1:]:
-            roomIdx = -1
-            # print('start:', start, end)
-            if room[0]<=start:
+        intervals.sort(key= lambda x:x[0])
+        for meet in intervals:
+            s, e = meet
+            if len(room) == 0:
+                heapq.heappush(room, e)
+                continue
+            topMeet = room[0]
+            if topMeet <= s:
                 heapq.heappop(room)
-            heapq.heappush(room, end)
-            # print(room)
+            heapq.heappush(room, e)
         return len(room)
-
-
-        
