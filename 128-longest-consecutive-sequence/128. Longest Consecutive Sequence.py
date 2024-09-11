7b5e794d8b93118a404248cd32591fe520c4b857
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-10 22:37:56 -0700

    Runtime: 342 ms (76.41%), Memory: 31.7 MB (82.26%) - Gitcode

--- 128-longest-consecutive-sequence/128. Longest Consecutive Sequence.py
+++ 128-longest-consecutive-sequence/128. Longest Consecutive Sequence.py
@@ -1,24 +1,27 @@
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # direct sorting is O(NlogN+N) time
-        # put the n as key to locate them fast
-        # by only checking the start of possible continue array
-        # we would atmost check the array twice: time: O(N) space:O(n) for set
-        numSet = set(nums)
-        ret = 0
-        for n in nums:
-            # to get the head of the continue array
-            if n-1 not in numSet:
-                curN = n
-                curStreak = 1
-                while curN+1 in numSet:
-                    curN +=1
-                    curStreak += 1
-                ret = max(ret, curStreak)
-        return ret
-            
-            
+        # if not limited to O(N), then sort to count=>O(NlogN+N)
+        # iterate the list, using a dict to keep track of the current groups
+        # check each num for it's prev and after to merge into the group
+        # only keep the boarder of the num in the list
+        # above self-idea hard to implement
 
+        # By only taking the start of the iteration to consider
+        # look for the combination of connected sequence
+        # runtime is O(N) as we only have the start of sequence look after
+        # O(N+N), with set lookup is O(1) for each operation
+        allNums = set(nums)
+        maxGroupSize = 0
+        for num in allNums:
+            # take only the start of a sequence
+            if num-1 not in allNums:
+                currentNum = num
+                currentSize = 1
+                while currentNum+1 in allNums:
+                    currentNum += 1
+                    currentSize +=1
+                maxGroupSize = max(maxGroupSize, currentSize)
 
+        return maxGroupSize
 
+        
