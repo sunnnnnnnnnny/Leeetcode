d1303cd0eb8747ae643259dcbd3c3f3b40d7cfd2
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-13 09:42:09 -0700

    Runtime: 38 ms (38.53%), Memory: 16.5 MB (76.69%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -1,18 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # as only move is down or right
-        # dp[i][j] = dp[i-1][j]+dp[i][j-1]
-        def dp(self, x:int, y:int) -> int:
-            if x==0 and y==0:
-                return 1
-            if x<0 or y<0:
-                return 0
-            key = str(x)+'_'+str(y)
-            if key in self.memo:
-                return self.memo[key]
-            uniWays = dp(self, x-1, y)+dp(self, x, y-1)
-            self.memo[key] = uniWays
-            return uniWays
-        self.memo = {}
-        # the grid size is larger than the index, thus need to minus 1
-        return dp(self, m-1, n-1)
+        # dp[i][j] = from up dp[i-1][j]+ from left dp[i][j-1]
+        # dp[i][j] will be the unique way for the robot to reach this location
+        # time: O(N*M) space:O(N*M)
+        # bottom-up with getting the direction from up to bottom and left to right
+        dp = [[0 for i in range(n) ] for j in range(m)]
+        # assume n&m>0 set up base condition
+        # could modify to save space with only record the previous row-O(N)
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
