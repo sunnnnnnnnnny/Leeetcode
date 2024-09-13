2bda768c48b1e0bf77669f2f65652b3372a1958f
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-12 23:46:16 -0700

    Runtime: 39 ms (35.48%), Memory: 16.5 MB (33.28%) - Gitcode

--- 213-house-robber-ii/213. House Robber II.py
+++ 213-house-robber-ii/213. House Robber II.py
@@ -1,34 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # dp[n:1] = max(n+dp[n-2:2], dp[n-1:1])
-        def dp(end, start):
-            if start>end:
-                return 0
-            if start == end:
-                return nums[start]
-            if end-start == 1:
-                return max(nums[start], nums[end])
-            # print('start: end:',start, end)
-            return max(nums[end]+dp(end-2,start), dp(end-1, start))
-        def dp_simple(self, nums:List[int])-> int:
-            if len(nums) == 0:
-                return 0
-            if len(nums) == 1:
-                return nums[0]
-            pre2 = nums[0]
-            pre1 = max(nums[0], nums[1])
-            now = max(nums[0], nums[1])
-            for i in range(2, len(nums), 1):
-                now = max(nums[i]+pre2, pre1)
-                pre2 = pre1
-                pre1 = now
-            return now
-        n= len(nums)-1
-        if n<1:
-            return nums[n]
-        # print(nums[1:n+1])
-        # print(nums[0:n])
-        # to break the tie of connected cycle of first and last house
-        dp_n_to_1 = dp_simple(self, nums[1:n+1])
-        dp_nm1_to_0 = dp_simple(self, nums[0:n])
-        return max(dp_n_to_1, dp_nm1_to_0)
+        # treat as the same with breaking down the house
+        # since house[1] house[n] are next to each other
+        # the max would be (1-n-1) or (2-n)
+        # dp[i] = max(dp[i-1], dp[i-2]+n[i])
+        # time:O(2N) = O(N) space:O(1) if we keep only the pre and pre-1
+        N = len(nums)
+        if N == 0:
+            return 0
+        elif N== 1:
+            return nums[0]
+        elif N==2:
+            return max(nums[0], nums[1])
+        
+        prev0 = nums[0]
+        prev1 = max(nums[0], nums[1])
+        maxSumN1 = max(prev0, prev1)
+        for i in range(2,N-1):
+            maxSumN1 = max(prev0+nums[i], prev1)
+            prev0 = prev1
+            prev1 = maxSumN1
+        
+        maxSumN2 = 0
+        prev0 = 0
+        prev1 = nums[1]
+        maxSumN2 = max(prev0, prev1)
+        for i in range(2,N):
+            maxSumN2 = max(prev0+nums[i], prev1)
+            prev0 = prev1
+            prev1 = maxSumN2
+        return max(maxSumN2, maxSumN1)
