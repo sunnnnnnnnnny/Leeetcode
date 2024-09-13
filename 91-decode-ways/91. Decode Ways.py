9019dfd3325d2a06de9843c83b88690727987ec7
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-13 00:07:13 -0700

    Runtime: 41 ms (22.60%), Memory: 16.6 MB (52.42%) - Gitcode

--- 91-decode-ways/91. Decode Ways.py
+++ 91-decode-ways/91. Decode Ways.py
@@ -1,27 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # each char can be group single or in two
-        # dp[i] = (if s[i-1:1] is valid)dp[i-1]+(if s[i-1:2] is valid)dp[i-2]
-        def isValidLetter(subS:str)->bool:
-            if len(subS)<1 or subS[0] == '0':
-                return False
-            # print(subS)
-            inDigit = int(subS)
-            if inDigit>0 and inDigit<27:
-                return True
-            return False
-        dpList = [0]*len(s)
-        dpList[0] = 1 if isValidLetter(s[0:1]) else 0
-        if dpList[0] == 0 or len(s)==1:
-            return dpList[0]
-        
-        # python substr is start:end
-        dpList[1] = dpList[0] if isValidLetter(s[1:2]) else 0
-        dpList[1] += 1 if isValidLetter(s[0:2]) else 0
-        for i in range(2, len(s), 1):
-            dpList[i] += dpList[i-1] if isValidLetter(s[i:i+1]) else 0
-            dpList[i] += dpList[i-2] if isValidLetter(s[i-1:i+1]) else 0
-            # print('i: ',i, dpList)
-        return dpList[len(s)-1]
-
-        
+        # dp[i] = dp[i-1](if not 0 and <9)+( if s[i-1:i] is valid )dp[i-2]
+        # time:O(N) space:O(N) but can be optimized as O(1)
+        N = len(s)
+        if N==0:
+            return 0
+        elif N==1:
+            return 0 if ord(s[0])-ord('0')==0 else 1
+        dp = [0 for i in range(N+1)]
+        dp[0] = dp[1] = 1
+        if ord(s[0])-ord('0')==0:
+            dp[1] = 0
+        print(dp)
+        for i in range(1,N):
+            if ord(s[i])-ord('0')>0 and ord(s[i])-ord('0')<10:
+                dp[i+1]+=dp[i]
+            # print(i, s[i-1:i+1], dp)
+            if ord(s[i-1])-ord('0')>0 and int(s[i-1:i+1])<=26:
+                dp[i+1]+=dp[i-1]
+        return dp[-1]
