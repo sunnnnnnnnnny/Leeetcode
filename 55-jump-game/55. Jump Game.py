b9d9fe45a13d0c079513ce4c4bb9191f71e4c2e9
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-13 10:11:20 -0700

    Runtime: 350 ms (68.58%), Memory: 17.7 MB (67.12%) - Gitcode

--- 55-jump-game/55. Jump Game.py
+++ 55-jump-game/55. Jump Game.py
@@ -1,47 +1,25 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # greedy similar to bottom-up from right concept
-        # if any position from left can reach the lastIdx
-        # meaning its good, so we update the lastIdx as it
+        # greedy, check if the any of the left most position can be reached
+        # no need to care if there's multiple way to get there
         lastIdx = len(nums)-1
-        for i in range(len(nums)-2, -1, -1):
-            if i+nums[i] >= lastIdx:
+        for i in range(len(nums)-2, -1,-1):
+            # as long as it can jump over lastIdx, then we are good
+            if i+nums[i]>=lastIdx:
                 lastIdx = i
-        return lastIdx==0
-
-
-        # bottom-up with memo which starting from the right most
-        # then update the previous step j can jump from i
-        # also stop checking more j when i is good
-        # cost: O(n^2) as we recursive at most 2 for loop
-        # Good, Bad, Unknown = 1, 0, -1
-        # canReverse = [Unknown]*len(nums)
-        # lastIdx = len(nums)-1
-        # canReverse[lastIdx] = Good
-        # for i in range(lastIdx-1, -1, -1):
-        #     furthestJumpIdx = min(i+nums[i], lastIdx)
-        #     for j in range(i+1, furthestJumpIdx+1, 1):
-        #         if canReverse[j] == Good:
-        #             canReverse[i] = Good
-        #             break
-            
-        # return canReverse[0] == Good
-
-        # bottom-up by recording if any step can be jumped on
-        # dp[i] = True if(dp[i-j] and i+nums[j] == i)
-        # canReach = [False]*(len(nums))
-        # lastIdx = len(nums)-1
-        # canReach[0] = True
+        return lastIdx == 0
+        # #  dp[i] meaning if we can jump to that location
+        # # dp[i] = true if j+jump[j]>i and dp[j]==true while i>j
+        # # or bottom up to populate the dp
+        # # time:O(N^N) space:O(N)
+        # dp = [0 for i in range(len(nums))]
+        # dp[0] = 1
         # for i in range(len(nums)):
-        #     if canReach[i]:
-        #         for j in range(1, nums[i]+1, 1):
-        #             # no need to care the rest
-        #             if i+j>=len(nums):
-        #                 break
-        #             canReach[i+j] = True
-        #             if i+j == lastIdx:
+        #     # d[i]>0 meaning we can be at this location
+        #     if dp[i]>0:
+        #         for j in range(1,nums[i]+1):
+        #             if i+j<len(nums):
+        #                 dp[i+j] = 1
+        #             if i+j==len(nums)-1:
         #                 return True
-
-        # # print(canReach)
-        # return canReach[len(nums)-1]
-        
+        # return True if dp[-1]>0 else False
