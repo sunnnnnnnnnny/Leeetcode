0914894d583ce53506ef50e44f7f957d33405fbe
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-15 10:18:44 -0700

    Runtime: 89 ms (61.22%), Memory: 22.2 MB (17.86%) - Gitcode

--- 49-group-anagrams/49. Group Anagrams.py
+++ 49-group-anagrams/49. Group Anagrams.py
@@ -1,23 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # sort the words, and check them as it's the key
-        # time:O(n*nlogn) n is the avg len of str
-        # space:O(N)
-        # go through each str cnt the freq and use it as the key, 
-        # group by the key, time:O(n*n) space:O(n)
-        # assume the character's only having 36 english character
-        # if we consider uppercase letters?
-        # also we assume we don't need to deduplicate
-        groupKey = {}
-        def getK(word):
-            keySet = [0 for _ in range(26)]
-            for i in range(len(word)):
-                char = ord(word[i])-ord('a')
-                keySet[char]+=1
-            return tuple(keySet)
+        # by counting the anagram of each words as key
+        # then group the words by key
+        # time: O(NK) for counting the key, K is the max len of str
+        # space: O(NK) record the keys, max length of the list length+word length
+        # 2. sort the words as the key, then group it
+        # time: O(NKlog(K)) Space:O(NK)
+        ans: DefaultDict[int, List[str]] = collections.defaultdict(list)
         for s in strs:
-            nowK = getK(s)
-            if nowK not in groupKey.keys():
-                groupKey[nowK] = []
-            groupKey[nowK].append(s)
-        return groupKey.values()
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            ans[tuple(count)].append(s)
+        return ans.values()
+        # thie is sort thus time cost higher
+        # anaGroup = {}
+        # from collections import Counter
+        # for eachStr in strs:
+        #     key = ''.join(sorted(eachStr))
+        #     if key not in anaGroup:
+        #         anaGroup[key] = []
+        #     anaGroup[key].append(eachStr)
+        # return anaGroup.values()
+
+        
