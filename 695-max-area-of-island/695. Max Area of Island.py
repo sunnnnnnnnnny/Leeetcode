43b913521ddfad2e7e174f52e290eb9a3d5ceef9
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-17 09:17:32 -0700

    Runtime: 91 ms (97.76%), Memory: 17.3 MB (41.86%) - Gitcode

--- 695-max-area-of-island/695. Max Area of Island.py
+++ 695-max-area-of-island/695. Max Area of Island.py
@@ -1,37 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        # traverse the whole grid, cound the area of island by dfs/bfs
-        # keep record of it
-        # time:O(2N)=O(N)
-        # space:O(N)
-        m = len(grid)
-        n = len(grid[0])
+        # use bfs or dfs for searching the new land of the whole island 
+        # keep track of the maxArea of all islands
+        # time: O(N*M*2) space:O(1) if we can modify grid inspace
+        N = len(grid)
+        M = len(grid[0])
         maxArea = 0
-        for i in range(m):
-            for j in range(n):
+        def bfs(nowX, nowY):
+            nonlocal N, M, grid
+            if grid[nowX][nowY] != 1:
+                return 0
+            grid[nowX][nowY] = 2
+            addArea = 1
+            if nowX-1>=0:
+                addArea+= bfs(nowX-1, nowY)
+            if nowX+1<N:
+                addArea+= bfs(nowX+1, nowY)
+            if nowY-1>=0:
+                addArea+= bfs(nowX, nowY-1)
+            if nowY+1<M:
+                addArea+= bfs(nowX, nowY+1)
+            return addArea
+        for i in range(N):
+            for j in range(M):
                 if grid[i][j] == 1:
-                    # bfs
-                    nextGrid = [(i,j)]
-                    grid[i][j] = 2
-                    curArea = 0
-                    while nextGrid:
-                        levelLen = len(nextGrid)
-                        for l in range(levelLen):
-                            now = nextGrid.pop(0)
-                            curArea += 1
-                            x,y = now[0], now[1]
-                            if x-1>=0 and grid[x-1][y] == 1:
-                                grid[x-1][y] = 2
-                                nextGrid.append((x-1,y))
-                            if x+1<m and grid[x+1][y] == 1:
-                                grid[x+1][y] = 2
-                                nextGrid.append((x+1,y))
-                            if y-1>=0 and grid[x][y-1] == 1:
-                                grid[x][y-1] = 2
-                                nextGrid.append((x,y-1))
-                            if y+1<n and grid[x][y+1] == 1:
-                                grid[x][y+1] = 2
-                                nextGrid.append((x,y+1))
+                    curArea = bfs(i,j)
                     maxArea = max(maxArea, curArea)
         return maxArea
-        
+
