d4b2ba9c1f4d5df8c0b2cf60f1156059e2cac98e
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-18 23:32:56 -0700

    Runtime: 637 ms (41.94%), Memory: 43.1 MB (78.15%) - Gitcode

--- 1146-snapshot-array/1146. Snapshot Array.py
+++ 1146-snapshot-array/1146. Snapshot Array.py
@@ -7,27 +7,50 @@ class SnapshotArray:
 # time:O(snap) space:O(snap*n) worst but can be O(N) 
     def __init__(self, length: int):
         self.len = length
-        # self.arr = [0 for _ in range(length)]
+        self.arr = [[] for _ in range(length)]
         self.snapCnt = 0
-        self.snapID2Mod = [{}]
+        # self.snapID2Mod = [{}]
 
     def set(self, index: int, val: int) -> None:
-        # self.arr[index] = val
-        self.snapID2Mod[self.snapCnt][index] = val 
+        if len(self.arr[index])>0:
+            lastSnap = self.arr[index][-1][0]
+            if lastSnap == self.snapCnt:
+                self.arr[index][-1][1] = val
+            else:
+                self.arr[index].append([self.snapCnt, val])
+        else:
+            self.arr[index].append([self.snapCnt, val])
+        
+        # self.snapID2Mod[self.snapCnt][index] = val 
         # print(self.snapCnt, self.snapID2Mod[self.snapCnt])
 
     def snap(self) -> int:
         self.snapCnt+=1
-        self.snapID2Mod.append({})
+        # self.snapID2Mod.append({})
         return self.snapCnt-1
         
     def get(self, index: int, snap_id: int) -> int:
         if snap_id>=self.snapCnt:
             return 0
-        for i in range(snap_id,-1,-1):
-            if index in self.snapID2Mod[i].keys():
-                return self.snapID2Mod[i][index]
-        return 0
+        # for i in range(snap_id,-1,-1):
+        #     if index in self.snapID2Mod[i].keys():
+        #         return self.snapID2Mod[i][index]
+        def binarySearch(indexArr, snap_id):
+            l = 0 
+            r = len(indexArr)-1
+            while l<=r:
+                mid = (l+r)//2
+                if indexArr[mid][0]==snap_id:
+                    return mid
+                if indexArr[mid][0]<snap_id:
+                    l = mid+1
+                else:
+                    r = mid-1
+            return r
+        idx = binarySearch(self.arr[index], snap_id)
+        # print(self.arr[index], snap_id, idx)
+        return self.arr[index][idx][1] if idx>=0 else 0
+        # return 0
         
 
 
