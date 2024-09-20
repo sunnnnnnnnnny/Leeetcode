d02c169c75fc0aaf8cb32cdceef955dacfe16504
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-20 00:28:31 -0700

    Runtime: 70 ms (79.09%), Memory: 19.9 MB (57.64%) - Gitcode

--- 253-meeting-rooms-ii/253. Meeting Rooms II.py
+++ 253-meeting-rooms-ii/253. Meeting Rooms II.py
@@ -1,18 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        # sort the time from start?
-        # record the occupied meeting room end time, with minHead
-        # use the first released meeting room
-        # time;O(NlogN+N) space:O(N)
-        room = []
-        intervals.sort(key= lambda x:x[0])
+        # sort the room with end time of increasing 
+        # using the minHeap for end time, if the next end>minHeap head
+        # then we need one more room, 
+        # time:O(NlogN+NlogK) K is the size of room
+        # space:O(N)
+        intervals.sort(key = lambda x:x[0])
+        meetEnd = []
         for meet in intervals:
-            s, e = meet
-            if len(room) == 0:
-                heapq.heappush(room, e)
-                continue
-            topMeet = room[0]
-            if topMeet <= s:
-                heapq.heappop(room)
-            heapq.heappush(room, e)
-        return len(room)
+            iEnd = meet[1]
+            if len(meetEnd)>0 and meetEnd[0]<=meet[0]:
+                heapq.heappop(meetEnd)
+            heapq.heappush(meetEnd, iEnd)
+        return len(meetEnd)
