0191e5889ff17114b315fb8fc350fe2062d71273
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-22 20:03:08 -0700

    Runtime: 38 ms (36.42%), Memory: 16.7 MB (6.49%) - Gitcode

--- 362-design-hit-counter/362. Design Hit Counter.py
+++ 362-design-hit-counter/362. Design Hit Counter.py
@@ -8,20 +8,43 @@ class HitCounter:
 # since it's chronological, we can get the past 300 sec record in queue
 # record the hits, time:O(300) = O(1) for any of it
     def __init__(self):
-        self.timeHitCnt = {}
+        # self.timeHitCnt = {}
         self.timeHitCache = {}
+        self.past5MinQ = []
+
 
     def hit(self, timestamp: int) -> None:
-        if timestamp not in self.timeHitCnt.keys():
-            self.timeHitCnt[timestamp] = 0
-        self.timeHitCnt[timestamp]+=1
+        # if timestamp not in self.timeHitCnt.keys():
+        #     self.timeHitCnt[timestamp] = 0
+        while len(self.past5MinQ)>0:
+            prevT, prevCnt = self.past5MinQ[0]
+            if prevT<=timestamp-300:
+                self.past5MinQ.pop(0)
+            else:
+                break
+        if len(self.past5MinQ) == 0  or self.past5MinQ[-1][0]!= timestamp:
+            self.past5MinQ.append([timestamp,1])
+        else:
+            self.past5MinQ[-1][1]+=1
+        # self.timeHitCnt[timestamp]+=1
 
     def getHits(self, timestamp: int) -> int:
-        ret = 0
-        for i in range(timestamp, timestamp-300, -1):
-            if i in self.timeHitCnt.keys():
-                ret += self.timeHitCnt[i]
-        return ret
+        while len(self.past5MinQ)>0:
+            prevT, prevCnt = self.past5MinQ[0]
+            if prevT<=timestamp-300:
+                self.past5MinQ.pop(0)
+            else:
+                break
+        
+        accumulateCnt = 0
+        for prevT, prevC in self.past5MinQ:
+            accumulateCnt+=prevC
+        return accumulateCnt
+        # ret = 0
+        # for i in range(timestamp, timestamp-300, -1):
+        #     if i in self.timeHitCnt.keys():
+        #         ret += self.timeHitCnt[i]
+        # return ret
 
 
 # Your HitCounter object will be instantiated and called as such:
