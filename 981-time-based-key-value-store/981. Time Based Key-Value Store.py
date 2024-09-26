53f28d98d93cd997f1752ab087d3545504e61ff2
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-09-26 10:14:08 -0700

    Runtime: 568 ms (77.58%), Memory: 74.8 MB (9.21%) - Gitcode

--- 981-time-based-key-value-store/981. Time Based Key-Value Store.py
+++ 981-time-based-key-value-store/981. Time Based Key-Value Store.py
@@ -1,27 +1,30 @@
 class TimeMap:
-
+# if the timestamp is not strick increasing, 
+# then we need to use binary search to find the insert position
+# method 1: keep the key as key with sorted by timestamp to val
+# method 2: keep the timestamp as key, and keep the item as key to val
+# time:O(logN+N)  space:O(N)
     def __init__(self):
-        self.timeKey = []
-        self.time2Val = {}
-#  time:O(logn+n) space:O(N)
+        self.key2TimeVal = {}
+
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if timestamp not in self.time2Val.keys():
-            self.time2Val[timestamp] = {}
-            insertIdx = bisect.bisect_left(self.timeKey, timestamp)
-            # print("set ",timestamp, insertIdx)
-            self.timeKey.insert(insertIdx, timestamp)
+        if key not in self.key2TimeVal:
+            self.key2TimeVal[key] = []
+        self.key2TimeVal[key].append([timestamp,value])
         
-        self.time2Val[timestamp][key] = value
-# time:O(logn+n)
+
     def get(self, key: str, timestamp: int) -> str:
-        checkIdx = bisect.bisect_left(self.timeKey, timestamp)
-        endCheckIdx = checkIdx if checkIdx<len(self.timeKey) else len(self.timeKey)-1
-        # print(self.timeKey)
-        # print(timestamp, checkIdx, endCheckIdx)
-        for i in range(endCheckIdx, -1, -1):
-            if self.timeKey[i]<=timestamp and key in self.time2Val[self.timeKey[i]].keys():
-                return self.time2Val[self.timeKey[i]][key]
-        return ""
+        if key not in self.key2TimeVal:
+            return ""
+        if timestamp<self.key2TimeVal[key][0][0]:
+            return ""
+        idx = bisect.bisect_left(self.key2TimeVal[key], timestamp, key=lambda x: x[0])
+        # print(self.key2TimeVal[key], idx, timestamp)
+        if idx>=len(self.key2TimeVal[key]):
+            idx = len(self.key2TimeVal[key])-1
+        if self.key2TimeVal[key][idx][0]>timestamp:
+            idx-=1
+        return self.key2TimeVal[key][idx][1]
         
 
 
