b740ccf7dd3c9bd65d4cdd7717aec6b251e412e2
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-06 10:24:18 -0700

    Runtime: 481 ms (42.20%), Memory: 19.9 MB (34.59%) - Gitcode

--- 542-01-matrix/542. 01 Matrix.py
+++ 542-01-matrix/542. 01 Matrix.py
@@ -4,27 +4,32 @@ class Solution:
         # time:O(N*M) space:O(N*M)
         n = len(mat)
         m = len(mat[0])
+        dist = [[-1 for i in range(m)] for j in range(n)]
         locQ = []
         for i in range(n):
             for j in range(m):
                 if mat[i][j] == 0:
                     locQ.append((i,j))
+                    dist[i][j] = 0
         level = 0
-        dist = [[-1 for i in range(m)] for j in range(n)]
         while locQ:
             levelSize = len(locQ)
             for i in range(levelSize):
                 nowi, nowj = locQ.pop(0)
-                if dist[nowi][nowj] != -1:
-                    continue
-                dist[nowi][nowj] = level
-                if nowi-1>=0 and mat[nowi-1][nowj]==1 and dist[nowi-1][nowj]==-1:
+                # if dist[nowi][nowj] != -1:
+                #     continue
+                # dist[nowi][nowj] = level
+                if nowi-1>=0 and dist[nowi-1][nowj]==-1:
+                    dist[nowi-1][nowj] = level+1
                     locQ.append((nowi-1,nowj))
-                if nowi+1<n and mat[nowi+1][nowj]==1 and dist[nowi+1][nowj]==-1:
+                if nowi+1<n and dist[nowi+1][nowj]==-1:
+                    dist[nowi+1][nowj] = level+1
                     locQ.append((nowi+1,nowj))
-                if nowj-1>=0 and mat[nowi][nowj-1]==1 and dist[nowi][nowj-1]==-1:
+                if nowj-1>=0 and dist[nowi][nowj-1]==-1:
+                    dist[nowi][nowj-1] = level+1
                     locQ.append((nowi,nowj-1))
-                if nowj+1<m and mat[nowi][nowj+1]==1 and dist[nowi][nowj+1]==-1:
+                if nowj+1<m and dist[nowi][nowj+1]==-1:
+                    dist[nowi][nowj+1] = level+1
                     locQ.append((nowi,nowj+1))
             level += 1
         return dist
