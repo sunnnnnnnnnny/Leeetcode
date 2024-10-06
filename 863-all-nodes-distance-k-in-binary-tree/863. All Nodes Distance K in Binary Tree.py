817e074d820098d25c70c5b97608020020fdb128
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-05 22:57:46 -0700

    Runtime: 45 ms (18.50%), Memory: 17 MB (14.61%) - Gitcode

---
+++ 863-all-nodes-distance-k-in-binary-tree/863. All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        # convert into a graph, then do bfs traversal
+        # time:O(2N) space:O(N) to record the graph
+        connect = {}
+        def dfs(now, par):
+            nonlocal connect
+            if now == None:
+                return
+            if now.val not in connect.keys():
+                connect[now.val] = []
+            if par != None:
+                connect[now.val].append(par.val)
+                connect[par.val].append(now.val)
+            dfs(now.left, now)
+            dfs(now.right,now)
+        dfs(root, None)
+        bfsQ = [target.val]
+        level = 0
+        visited = set()
+        ret = []
+        while bfsQ:
+            levelSize = len(bfsQ)
+            for i in range(levelSize):
+                now = bfsQ.pop(0)
+                if now in visited:
+                    continue
+                visited.add(now)
+                if level == k:
+                    ret.append(now)
+                for c in connect[now]:
+                    if c not in visited:
+                        bfsQ.append(c)
+            level+=1
+
+        return ret
+                
+
