f75ebf85e1f7610334dcd8a47024a00ee1b91127
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-06 09:47:47 -0700

    Runtime: 305 ms (19.39%), Memory: 18.3 MB (19.63%) - Gitcode

--- 875-koko-eating-bananas/875. Koko Eating Bananas.py
+++ 875-koko-eating-bananas/875. Koko Eating Bananas.py
@@ -1,65 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # the min of speed should not be the min(pile) should be 1
-        # koko speed k is min(piles)-max(piles)
-        # since we eat no more than one pile, 
-        # thus we have to find out what's the min k of it by helper functio
-        def canFinish(k:int)->bool:
-            needHour = 0
-            for pile in piles:
-                needHour += math.ceil(pile/k)
-            if needHour<=h:
-                return True
-            return False
-        left = 1
+        # binary search to get the speend 1, max(pile)
+        # time:O(log(max_pile)) space:O(1)
+        def canFinish(eatSp):
+            nonlocal piles, h
+            usedH = 0
+            for p in piles:
+                usedH += (p//eatSp)
+                if p%eatSp>0:
+                    usedH+=1
+            # print(eatSp, usedH)
+            return usedH<=h
         right = max(piles)
-        while left<=right:
-            mid = left + (right-left)//2
-            # print(left, right, mid)
+        left = 1
+        while left<right:
+            mid = (left+right)//2
             if canFinish(mid):
-                right = mid-1
+                right = mid
             else:
                 left = mid+1
-        return left
-
-
-        # the speed will in btw of 1 and max() of the banana piles
-        # need to check if it can be finished in h hours
-        # binary search for the speed -> time O(NlogM) space:O(1)
-        # def canFinishBanana(k):
-        #     useHour = 0
-        #     for pile in piles:
-        #         useHour += math.ceil(pile/k)
-        #     if useHour>h:
-        #         return False
-        #     return True
-        # left = 1
-        # right = max(piles)
-        # while left<right:
-        #     k = left+(right-left)//2
-        #     if canFinishBanana(k):
-        #         right = k
-        #     else:
-        #         left = k+1
-        # return right
-        # brute force is trying from speed 1 to max(pile[i])
-        # time: O(nm) space:O(1)
-        # binary search for 1...m
-        # time: O(n*log(m)) space:O(1)
-        # def finishBana(k:int):
-        #     useHour = 0
-        #     for pile in piles:
-        #         useHour += math.ceil(pile/k)
-        #     if useHour <= h:
-        #         return True
-        #     return False
-        # left = 1
-        # right = max(piles)
-        # while left< right:
-        #     mid = (left+right)//2
-        #     if finishBana(mid):
-        #         right = mid
-        #     else:
-        #         left = mid+1
-        # return right
-
+        return right
+        
