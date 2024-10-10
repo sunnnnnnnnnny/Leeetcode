693ddbc7e464a7e031c43ab5f506632529c6766b
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-10 00:14:38 -0700

    Runtime: 712 ms (61.44%), Memory: 34.6 MB (54.75%) - Gitcode

--- 2611-mice-and-cheese/2611. Mice and Cheese.py
+++ 2611-mice-and-cheese/2611. Mice and Cheese.py
@@ -5,34 +5,14 @@ class Solution:
         # then if not enough, choose the rest top reward1, with pority queue
         # time: O(nLogn) space:O(n)
         n = len(reward1)
-        reward1More = []
-        reward2More = []
-        for i in range(n):
-            diff = abs(reward1[i]-reward2[i])
-            if reward1[i]>reward2[i]:
-                heapq.heappush(reward1More, (-diff, i))
-            else:
-                heapq.heappush(reward2More, (-diff, i))
-        leftK = k
+        # sort by the difference of 1-2, then we can get the max points
+        # as to select the first k of it
+        diffOneVsTwo = [(reward1[i]-reward2[i], reward1[i], reward2[i]) for i in range(n)]
+        diffOneVsTwo.sort(reverse=True, key = lambda x:x[0])
         ret = 0
-        while reward1More and leftK>0:
-            diff, idx = heapq.heappop(reward1More)
-            ret += reward1[idx]
-            leftK -= 1
-        # if the reward1>reward2 doens't fill up the k, then we take the max reward2
-        if leftK>0:
-            while len(reward2More)>leftK:
-                diff, idx = heapq.heappop(reward2More)
-                ret += reward2[idx]
-            while reward2More:
-                diff, idx = heapq.heappop(reward2More)
-                ret += reward1[idx]
-        # if leftK == 0, the rest should all be eatenn by mice2
-        while reward1More:
-            diff, idx = heapq.heappop(reward1More)
-            ret += reward2[idx]
-        while reward2More:
-            diff, idx = heapq.heappop(reward2More)
-            ret += reward2[idx]
+        for i in range(k):
+            ret += diffOneVsTwo[i][1]
+        
+        for i in range(k,n):
+            ret += diffOneVsTwo[i][2]
         return ret
-
