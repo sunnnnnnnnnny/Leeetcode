e820f8b526077b548a19c0f39f55e72cda42fe74
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-11 19:36:27 -0700

    Runtime: 490 ms (56.64%), Memory: 42.5 MB (29.78%) - Gitcode

--- 1143-longest-common-subsequence/1143. Longest Common Subsequence.py
+++ 1143-longest-common-subsequence/1143. Longest Common Subsequence.py
@@ -1,29 +1,14 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # since dp[i][j] = lcs of str1[0:i] & str2[0:j]
-        # dp[i][j] = dp[i-1][j-1]+1 if str1[i] == str2[j]
-        # else max(dp[i-1][j], dp[i][j-1])
-        # time: O(M*N)
-        # memo = [[-1]*len(text2)]*len(text1)
-        m = len(text1)
-        n = len(text2)
-        memo = [[-1 for i in range(n)] for j in range(m)]
-        # memo[0][0] = 1 if text1[0] == text2[0] else 0
-        # print(memo)
-        def lcs(self, str1, str2, i, j, memo):
-            if i<0 or j<0:
-                return 0
-            # NOTE: python will allow neg idx, thus this always block the correct computation
-            if memo[i][j] >= 0:
-                return memo[i][j]
-            # print('i: j:, str:', i, j, str1[i], str2[j])
-            if str1[i] == str2[j]:
-                memo[i][j] = 1+lcs(self, str1, str2, i-1, j-1, memo)
-            else:
-                memo[i][j] = max(lcs(self, str1, str2, i-1, j, memo), lcs(self, str1, str2, i, j-1, memo))
-            
-            return memo[i][j]
-        
-        ret =  lcs(self, text1, text2, len(text1)-1, len(text2)-1, memo)
-        # print(memo)
-        return ret
+        # using the 2D grid to caculate the lcs length
+        # time:O(n*M) space:O(n*m)
+        n = len(text1)
+        m = len(text2)
+        dp = [[ 0 for i in range(m+1)] for j in range(n+1)]
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1+dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
