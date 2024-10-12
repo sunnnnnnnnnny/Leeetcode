1c46f479ee892b507304b31bcf2ae821039a2a8a
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-12 00:14:41 -0700

    Runtime: 765 ms (50.13%), Memory: 32.2 MB (31.66%) - Gitcode

--- 1944-number-of-visible-people-in-a-queue/1944. Number of Visible People in a Queue.py
+++ 1944-number-of-visible-people-in-a-queue/1944. Number of Visible People in a Queue.py
@@ -3,21 +3,25 @@ class Solution:
         # traverse from the left to right, with recording only the height when it's increasing 
         # record the height and it's seeing people with prority queue
         # pop until the vision is blocked, and record it back
-        # time:O(NlogN) space:O(N)
+        # time:O(NlogN) space:O(N), no need of prority qque
+        # using stack as the implement, time:O(N) space:O(N)
         n = len(heights)
         ret = [0 for i in range(n)]
         heightQ = []
-        heapq.heappush(heightQ, heights[n-1])
+        # heapq.heappush(heightQ, heights[n-1])
+        heightQ.append(heights[n-1])
         for i in range(n-2,-1,-1):
             nowView = 0
             while heightQ:
-                prevH = heightQ[0]
+                prevH = heightQ[-1]
                 if prevH<=heights[i]:
                     nowView+=1
-                    heapq.heappop(heightQ)
+                    # heapq.heappop(heightQ)
+                    heightQ.pop()
                 else:
                     nowView+=1
                     break
-            heapq.heappush(heightQ, heights[i])
+            # heapq.heappush(heightQ, heights[i])
+            heightQ.append(heights[i])
             ret[i] = nowView
         return ret
