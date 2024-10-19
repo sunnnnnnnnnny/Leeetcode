33f4acef85bd9704e5060164e5850906a6412b49
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-18 23:13:05 -0700

    Runtime: 0 ms (100.00%), Memory: 16.9 MB (12.31%) - Gitcode

--- 362-design-hit-counter/362. Design Hit Counter.py
+++ 362-design-hit-counter/362. Design Hit Counter.py
@@ -1,50 +1,25 @@
 class HitCounter:
-# since we only need to check the past 5 min = 300 sec of the record
-# we use hashMap for record the hit at each sec
-# getHits will go through the past 300 sec to accumulate the hit
-# time:O(1) for hit, getHit will be O(300) = O(1)
-# space:O(N) depending on the unique timestampes
-# method 2 by using queue FIFO for the timestamp
-# since it's chronological, we can get the past 300 sec record in queue
-# record the hits, time:O(300) = O(1) for any of it
+# since the timestamp is monotonically increasing, we could use a queue 
+# to record the past 300 second up items
+# time:O(N) for each check of the past 300 getHits
+# space:O(N)
     def __init__(self):
-        # self.timeHitCnt = {}
-        self.timeHitCache = {}
-        self.past5MinQ = []
-
+        self.pastHit = []
 
     def hit(self, timestamp: int) -> None:
-        # if timestamp not in self.timeHitCnt.keys():
-        #     self.timeHitCnt[timestamp] = 0
-        while len(self.past5MinQ)>0:
-            prevT, prevCnt = self.past5MinQ[0]
-            if prevT<=timestamp-300:
-                self.past5MinQ.pop(0)
-            else:
-                break
-        if len(self.past5MinQ) == 0  or self.past5MinQ[-1][0]!= timestamp:
-            self.past5MinQ.append([timestamp,1])
-        else:
-            self.past5MinQ[-1][1]+=1
-        # self.timeHitCnt[timestamp]+=1
+        self.pastHit.append(timestamp)
+        
 
     def getHits(self, timestamp: int) -> int:
-        while len(self.past5MinQ)>0:
-            prevT, prevCnt = self.past5MinQ[0]
-            if prevT<=timestamp-300:
-                self.past5MinQ.pop(0)
+        beginT = timestamp-300
+        while self.pastHit:
+            firstT = self.pastHit[0]
+            if firstT<=beginT:
+                self.pastHit.pop(0)
             else:
                 break
+        return len(self.pastHit)
         
-        accumulateCnt = 0
-        for prevT, prevC in self.past5MinQ:
-            accumulateCnt+=prevC
-        return accumulateCnt
-        # ret = 0
-        # for i in range(timestamp, timestamp-300, -1):
-        #     if i in self.timeHitCnt.keys():
-        #         ret += self.timeHitCnt[i]
-        # return ret
 
 
 # Your HitCounter object will be instantiated and called as such:
