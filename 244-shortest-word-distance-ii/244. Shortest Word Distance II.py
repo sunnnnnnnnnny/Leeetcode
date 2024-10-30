660b38cf4dcd86c2492ff506c3623ac4cea18fdd
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-30 00:16:23 -0700

    Runtime: 15 ms (28.72%), Memory: 24.9 MB (8.71%) - Gitcode

--- 244-shortest-word-distance-ii/244. Shortest Word Distance II.py
+++ 244-shortest-word-distance-ii/244. Shortest Word Distance II.py
@@ -10,31 +10,47 @@ class WordDistance:
             self.word2Idx[w].append(idx)
 
     def shortest(self, word1: str, word2: str) -> int:
-        # use the shorter one to go through it's checking from the other array
-        # time: O(n*M) O(N*logM)
-        shortW = word1
-        longW = word2
-        if len(self.word2Idx[word1])>len(self.word2Idx[word2]):
-            shortW = word2
-            longW = word1
-        # print(shortW, longW)
-        # print(self.word2Idx[shortW])
-        # print(self.word2Idx[longW])
-
+        # directly use the 2 idx array, for getting the distance by chaning the smaller index
+        # of each word list, time:O(N+M)
+        idx1 = 0
+        idx2 = 0
         ret = abs(self.word2Idx[word1][0]-self.word2Idx[word2][0])
-        for i in range(len(self.word2Idx[shortW])):
-            checkIdx = self.word2Idx[shortW][i]
-            lIdx = bisect.bisect_left(self.word2Idx[longW], checkIdx)
-            if lIdx >= len(self.word2Idx[longW]):
-                lIdx = len(self.word2Idx[longW])-1
-            frontDist = abs(checkIdx - self.word2Idx[longW][lIdx])
-            ret = min(ret, frontDist)
-            # print(checkIdx, lIdx)
-            lIdx -= 1
-            if lIdx >= 0:
-                frontDist = abs(checkIdx - self.word2Idx[longW][lIdx])
-                ret = min(ret, frontDist)
+        while idx1<len(self.word2Idx[word1]) and idx2<len(self.word2Idx[word2]):
+            nowDist = abs(self.word2Idx[word1][idx1]-self.word2Idx[word2][idx2])
+            ret = min(ret, nowDist)
+            
+            if self.word2Idx[word1][idx1]<self.word2Idx[word2][idx2]:
+                idx1 += 1
+            else:
+                idx2 += 1
         return ret
+
+
+        # use the shorter one to go through it's checking from the other array
+        # time: O(N*logM)
+        # shortW = word1
+        # longW = word2
+        # if len(self.word2Idx[word1])>len(self.word2Idx[word2]):
+        #     shortW = word2
+        #     longW = word1
+        # # print(shortW, longW)
+        # # print(self.word2Idx[shortW])
+        # # print(self.word2Idx[longW])
+
+        # ret = abs(self.word2Idx[word1][0]-self.word2Idx[word2][0])
+        # for i in range(len(self.word2Idx[shortW])):
+        #     checkIdx = self.word2Idx[shortW][i]
+        #     lIdx = bisect.bisect_left(self.word2Idx[longW], checkIdx)
+        #     if lIdx >= len(self.word2Idx[longW]):
+        #         lIdx = len(self.word2Idx[longW])-1
+        #     frontDist = abs(checkIdx - self.word2Idx[longW][lIdx])
+        #     ret = min(ret, frontDist)
+        #     # print(checkIdx, lIdx)
+        #     lIdx -= 1
+        #     if lIdx >= 0:
+        #         frontDist = abs(checkIdx - self.word2Idx[longW][lIdx])
+        #         ret = min(ret, frontDist)
+        # return ret
 
 
 
