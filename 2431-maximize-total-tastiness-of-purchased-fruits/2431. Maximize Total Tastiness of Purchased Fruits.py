0d749a8a54487e3ffeeafcdae8b6f074d4bad672
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-01 00:41:44 -0700

    Runtime: 747 ms (59.09%), Memory: 141.3 MB (39.31%) - Gitcode

--- 2431-maximize-total-tastiness-of-purchased-fruits/2431. Maximize Total Tastiness of Purchased Fruits.py
+++ 2431-maximize-total-tastiness-of-purchased-fruits/2431. Maximize Total Tastiness of Purchased Fruits.py
@@ -2,13 +2,16 @@ class Solution:
     def maxTastiness(self, price: List[int], tastiness: List[int], maxAmount: int, maxCoupons: int) -> int:
         n = len(price)
         ret = 0
+        memo = {}
         # dp = [[0]*(maxCoupons+1)]*(maxAmount+1)
         # without memo, time:O(2^n)
-        @cache
+        # @cache
         def bkV1(idx, leftAmount, leftCoupon):
-            nonlocal n, ret, price, tastiness
+            nonlocal n, ret, price, tastiness, memo
             if idx == n:
                 return 0
+            if (idx, leftAmount, leftCoupon) in memo:
+                return memo[(idx,leftAmount, leftCoupon)]
             nowMax = 0
             nowMax = max(nowMax, bkV1(idx+1, leftAmount, leftCoupon))
             if price[idx]<=leftAmount:
@@ -16,7 +19,7 @@ class Solution:
             
             if price[idx]//2<=leftAmount and leftCoupon>0:
                 nowMax = max(nowMax,tastiness[idx]+bkV1(idx+1, leftAmount-(price[idx]//2), leftCoupon-1))
-            
+            memo[(idx,leftAmount, leftCoupon)] = nowMax
             return nowMax
         ret = bkV1(0,maxAmount, maxCoupons)
         # def bk(idx, curTaste, leftAmount, leftCoupon):
