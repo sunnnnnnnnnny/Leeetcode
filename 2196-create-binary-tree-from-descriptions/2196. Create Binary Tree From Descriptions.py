f7f4a3287b1f66dedbd6128c6843471d8c414012
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-10-31 22:30:07 -0700

    Runtime: 330 ms (6.59%), Memory: 25 MB (90.15%) - Gitcode

---
+++ 2196-create-binary-tree-from-descriptions/2196. Create Binary Tree From Descriptions.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
+        # root will be only appearing in parent
+        # count the edge connected as constrait, then we can get the root and build it
+        # time: O(2N) space:O(N)
+        num2Par = defaultdict(list)
+        num2ParEdge = defaultdict(int)
+        root = set()
+        for des in descriptions:
+            par, ch, isLeft = des
+            num2Par[par].append(des)
+            num2ParEdge[ch] += 1
+            if num2ParEdge[par] == 0:
+                root.add(par)
+            if ch in root:
+                root.remove(ch)
+        # expect valid binary tree should only have one root
+        actRoot = None
+        for x in root:
+            now = TreeNode(x)
+            nextQ = [(x,now)]
+            while nextQ:
+                level = len(nextQ)
+                for i in range(level):
+                    nowN, parNode = nextQ.pop(0)
+                    # get child
+                    for chList in num2Par[nowN]:
+                        par, ch, isLeft = chList
+                        nextCh = TreeNode(ch)
+                        if isLeft == 1:
+                            parNode.left = nextCh
+                        else:
+                            parNode.right = nextCh
+                        nextQ.append((ch, nextCh))
+
+            actRoot = now 
+        return actRoot
