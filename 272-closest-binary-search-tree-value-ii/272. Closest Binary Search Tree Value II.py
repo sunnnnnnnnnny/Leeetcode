891d474bafd3c6b7543f597190c1e4bdf2ab7419
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-01 19:15:55 -0700

    Runtime: 0 ms (100.00%), Memory: 18.7 MB (26.08%) - Gitcode

--- 272-closest-binary-search-tree-value-ii/272. Closest Binary Search Tree Value II.py
+++ 272-closest-binary-search-tree-value-ii/272. Closest Binary Search Tree Value II.py
@@ -8,27 +8,29 @@ class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
         # maxStack of size K to keep record of the dist, val
         # or using the BST with pre-order then we can get the k
-        # time:O(N) space:O(N)
+        # time:O(N) space:O(N+K)
         ret = []
         def inOrder(now):
             nonlocal target, ret,k
             if now == None:
                 return
             inOrder(now.left)
-            nowDist = abs(now.val-target)
-            # print(now.val, nowDist)
-            if len(ret)>=k:
-                maxDist, val = ret[0]
+            ret.append(now.val)
+            if len(ret)>k:
+                head = abs(ret[0]-target)
+                last = abs(ret[-1]-target)
                 # print(now.val, maxDist, val)
-                if nowDist<-maxDist:
-                    heapq.heappop(ret)
-                    heapq.heappush(ret, (-nowDist, now.val))
-            else:
-                heapq.heappush(ret, (-nowDist, now.val))
+                # if the last distance is further than the first, then any of the is going to be bigger
+                # thus we can skip it
+                if last>head:
+                    ret.pop()
+                    return
+                else:
+                    ret.pop(0)
             inOrder(now.right)
         inOrder(root)
-        ans = []
-        while ret:
-            dist, num = heapq.heappop(ret)
-            ans.append(num)
-        return ans
+        # ans = []
+        # while ret:
+        #     dist, num = heapq.heappop(ret)
+        #     ans.append(num)
+        return ret
