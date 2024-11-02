aa7e5527c4c8dca0e82362f535adf473c2d6bf71
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-02 00:52:00 -0700

    Runtime: 62 ms (17.24%), Memory: 18 MB (15.04%) - Gitcode

--- 1188-design-bounded-blocking-queue/1188. Design Bounded Blocking Queue.py
+++ 1188-design-bounded-blocking-queue/1188. Design Bounded Blocking Queue.py
@@ -1,28 +1,29 @@
-# from threading import lock
 import threading
 class BoundedBlockingQueue(object):
 
     def __init__(self, capacity: int):
-        self.pushing = threading.Semaphore(capacity)
-        self.pulling = threading.Semaphore(0)
-        self.Q= []
-        self.p_lock = threading.Lock()
+        self.cap = capacity
+        self.push = threading.Lock()
+        self.pull = threading.Lock()
+        self.Q = []
+        self.pull.acquire()
 
     def enqueue(self, element: int) -> None:
-        self.pushing.acquire()
-        self.p_lock.acquire()
+        self.push.acquire()
         self.Q.append(element)
-        self.p_lock.release()
-        self.pulling.release()
+        if self.pull.locked():
+            self.pull.release()
+        if len(self.Q)<self.cap:
+            self.push.release()
 
     def dequeue(self) -> int:
-        self.pulling.acquire()
-        self.p_lock.acquire()
+        self.pull.acquire()
         ret = self.Q.pop(0)
-        self.p_lock.release()
-        self.pushing.release()
+        if self.push.locked():
+            self.push.release()
+        if len(self.Q)>0:
+            self.pull.release()
         return ret
 
     def size(self) -> int:
-        ret = len(self.Q)
-        return ret
+        return len(self.Q)
