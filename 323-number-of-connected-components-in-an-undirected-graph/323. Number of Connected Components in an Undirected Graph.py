cc88cba994a3bc4e73821b239cb7a0b7cd501af5
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-02 23:56:33 -0700

    Runtime: 7 ms (52.85%), Memory: 18.4 MB (57.61%) - Gitcode

--- 323-number-of-connected-components-in-an-undirected-graph/323. Number of Connected Components in an Undirected Graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/323. Number of Connected Components in an Undirected Graph.py
@@ -1,26 +1,29 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # by dfs traversal we can explore all the  connected nodes
-        # count the not connected ones
-        # time:O(N+M) space:O(N+M)
-        adjList = [[]for i in range(n)]
+        # union find, then count the node which the root is itself
+        # time:O(M+N) space:O(N)
+        # build the connection, go through the bfs of each node
+        # time:O(M+N) space:O(N*M)
+        visited = [0]*n
+        connect = defaultdict(set)
         for e in edges:
-            a,b = e
-            adjList[a].append(b)
-            adjList[b].append(a)
-        visited = set()
-        def dfs(now):
-            nonlocal visited
-            if now in visited:
-                return
-            visited.add(now)
-            for neighbor in adjList[now]:
-                if neighbor not in visited:
-                    dfs(neighbor)
-            return
-        connectComp = 0
+            a, b = e
+            connect[a].add(b)
+            connect[b].add(a)
+        
+        group = 0
         for i in range(n):
-            if i not in visited:
-                connectComp +=1
-                dfs(i)
-        return connectComp
+            if visited[i]==0:
+                group += 1
+                visited[i] = 1
+                bfsQ = [i]
+                while bfsQ:
+                    leSize = len(bfsQ)
+                    for j in range(leSize):
+                        now = bfsQ.pop(0)
+                        if now in connect.keys():
+                            for nowNext in connect[now]:
+                                if visited[nowNext] == 0:
+                                    bfsQ.append(nowNext)
+                                    visited[nowNext] = 1
+        return group
