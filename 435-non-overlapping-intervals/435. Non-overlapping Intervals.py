6300cd3d328406b0465ab984edbe6cbc754c1717
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-03 18:54:50 -0800

    Runtime: 84 ms (61.99%), Memory: 51 MB (75.18%) - Gitcode

--- 435-non-overlapping-intervals/435. Non-overlapping Intervals.py
+++ 435-non-overlapping-intervals/435. Non-overlapping Intervals.py
@@ -1,17 +1,13 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # sort the interval by end increasing
-        # b/c the earlier it ends, we can take more intecval
-        # greedy taking the possible interval, and remove any overlapped one
-        # time:O(NlogN+N) space:O(N)
-        intervals.sort(key=lambda x:x[1])
-        prevEnd = intervals[0][1]
-        removeCnt = 0
-        # print(intervals)
-        for i in range(1,len(intervals)):
-            if intervals[i][0]<prevEnd:
-                # overlapp
-                removeCnt+=1
+        # sort with the end then any non-overlapped is the good one
+        # time:O(nlogn) space:O(1)
+        intervals.sort(key = lambda x:x[1])
+        prev = intervals[0][1]
+        ret = 0
+        for i in range(1, len(intervals)):
+            if intervals[i][0]<prev:
+                ret += 1
             else:
-                prevEnd = intervals[i][1]
-        return removeCnt
+                prev = intervals[i][1]
+        return ret
