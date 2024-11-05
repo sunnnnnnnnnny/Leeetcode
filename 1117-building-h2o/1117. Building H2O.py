fee7fef2138969bd75e058a1f44596638f3db687
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-05 00:47:32 -0800

    Runtime: 53 ms (39.20%), Memory: 17.1 MB (80.59%) - Gitcode

--- 1117-building-h2o/1117. Building H2O.py
+++ 1117-building-h2o/1117. Building H2O.py
@@ -1,23 +1,22 @@
-from threading import Semaphore, Barrier
 class H2O:
     def __init__(self):
-        self.allowHy = Semaphore(2)
-        self.allowOx = Semaphore(1)
-        self.needConntent = Barrier(3)
+        self.bar = threading.Barrier(3)
+        self.h2Lock = threading.Semaphore(2)
+        self.oLock = threading.Semaphore(1)
         pass
 
 
     def hydrogen(self, releaseHydrogen: 'Callable[[], None]') -> None:
-        self.allowHy.acquire()
-        self.needConntent.wait()
+        self.h2Lock.acquire()
+        self.bar.wait()
         # releaseHydrogen() outputs "H". Do not change or remove this line.
         releaseHydrogen()
-        self.allowHy.release()
+        self.h2Lock.release()
 
 
     def oxygen(self, releaseOxygen: 'Callable[[], None]') -> None:
-        self.allowOx.acquire()
-        self.needConntent.wait()
+        self.oLock.acquire()
+        self.bar.wait()
         # releaseOxygen() outputs "O". Do not change or remove this line.
         releaseOxygen()
-        self.allowOx.release()
+        self.oLock.release()
