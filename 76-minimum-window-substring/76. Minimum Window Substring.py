0b1cb0b212ddd64decdeb244a08591169ee3d402
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-13 09:47:26 -0800

    Runtime: 398 ms (14.49%), Memory: 17.3 MB (30.24%) - Gitcode

--- 76-minimum-window-substring/76. Minimum Window Substring.py
+++ 76-minimum-window-substring/76. Minimum Window Substring.py
@@ -1,37 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # start to expand the window until it's valid to cover the whole t, then shrink it to min,
-        # then move the window with this size, once find another valid window start to shrink
-        # char in t is m max 26, s size is n. time:O(s+t) space:O(s+t)
-        hashT = collections.Counter(t)
+        # sliding window, expand if the substr doesn't cover thewindow
+        # once it covers shrink the window until it's not valid
+        # time:O(N) space:O(N) for keeping the record to verify substr validate
         start = 0
-        ret = ""
-        minLen = len(s)
-        def validCover(hashT):
-            leftNeedCharCnt = 0
-            for v in hashT.values():
-                leftNeedCharCnt += v if v>=0 else 0
-            return True if leftNeedCharCnt == 0 else False
-        for i in range(len(s)):
-            c = s[i]
-            if c in hashT:
-                hashT[c] -= 1
-            if validCover(hashT):
-                # try to shrink
-                while start<i:
-                    startC = s[start]
-                    if startC in hashT:
-                        hashT[startC] += 1
-                    if validCover(hashT):
-                        start += 1
-                    else:
-                        if startC in hashT:
-                            hashT[startC] -= 1
-                        break
-                nowLen = (i-start+1)
-                ret = s[start:i+1] if minLen>=nowLen else ret
-                minLen = min(minLen, nowLen)
-            else:
-                if ret != "":
-                    start += 1
-        return ret
+        subCharCnt = {}
+        for i in range(len(t)):
+            if t[i] not in subCharCnt.keys():
+                subCharCnt[t[i]] = 0
+            subCharCnt[t[i]]+=1
+        def subStrValid(cntDict):
+            for key, val in cntDict.items():
+                if val>0:
+                    return False
+            return True
+        minLenStr = ""
+        for end in range(len(s)):
+            if s[end] in subCharCnt.keys():
+                    subCharCnt[s[end]]-=1
+            while subStrValid(subCharCnt) and start<=end:
+                curSubStr = s[start:end+1]
+                if minLenStr == "" or len(minLenStr)>len(curSubStr):
+                    minLenStr = curSubStr
+                if s[start] in subCharCnt.keys():
+                    subCharCnt[s[start]]+=1
+                start +=1
+        return minLenStr
+            
+
