604a28fbe632354f3d52785acee85bdd72068cbb
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-20 23:18:08 -0800

    Runtime: 3 ms (31.23%), Memory: 16.6 MB (31.21%) - Gitcode

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974. Minimum Time to Type Word Using Special Typewriter.py
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974. Minimum Time to Type Word Using Special Typewriter.py
@@ -2,13 +2,11 @@ class Solution:
     def minTimeToType(self, word: str) -> int:
         # get the min dist from current to next, with the start of a
         # time:O(N) space:O(1)
-        curChar = 'a'
         def minDist(cur, want):
             clockWise = abs(ord(want)-ord(cur))
             countClock = 26-clockWise
             return min(clockWise, countClock)
-        ret = len(word)
-        for i in range(len(word)):
-            ret += minDist(curChar, word[i])
-            curChar = word[i]
+        ret = len(word)+minDist('a', word[0])
+        for i in range(1, len(word)):
+            ret += minDist(word[i-1], word[i])
         return ret
