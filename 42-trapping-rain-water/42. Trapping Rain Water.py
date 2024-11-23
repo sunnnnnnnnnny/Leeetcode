ec24f2cb852e082755b97511da60d5c9299701d6
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-23 10:38:43 -0800

    Runtime: 31 ms (24.67%), Memory: 18.2 MB (98.40%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,21 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        # bruteforce by setting each point as start, check the nearest hiehgt for it
-        # so we know it can trap the water
+        # get the hight from left and right, for each idx, the accumulated water is min(left, right)
+        # time:O(N) space:O(N)
         n = len(height)
-        # find the prefixLeft and prefixRight of trapped point then go to both end
-        # time:O(N) space:O(2N) = O(N)
-        maxLeft = [0]*n
-        maxRight = [0]*n
-        rIdx = n-1
-        maxLeft[0] = height[0]
-        maxRight[rIdx] = height[rIdx]
-        for i in range(1,n):
-            rIdx -= 1
-            maxRight[rIdx] = max(height[rIdx], maxRight[rIdx+1])
-            maxLeft[i] = max(height[i], maxLeft[i-1])
+        rightHeight = [0] * n
+        for i in range(n-2, -1, -1):
+            rightHeight[i] = max(rightHeight[i+1], height[i+1])
         ret = 0
-        for i in range(1,n-1):
-            if maxRight[i]>height[i] and maxLeft[i]>height[i]:
-                ret += min(maxRight[i], maxLeft[i])-height[i]
+        leftH = 0
+        for i in range(1, n):
+            leftH = max(leftH, height[i-1])
+            nowW = min(leftH, rightHeight[i])
+            ret += nowW-height[i] if nowW>height[i] else 0
         return ret
+        
