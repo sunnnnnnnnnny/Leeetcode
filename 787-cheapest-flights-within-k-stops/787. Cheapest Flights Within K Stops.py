a03d21b62607c1336ad0666f2ef22cb8c982e7fc
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-24 10:17:07 -0800

    Runtime: 75 ms (33.55%), Memory: 17.8 MB (82.45%) - Gitcode

--- 787-cheapest-flights-within-k-stops/787. Cheapest Flights Within K Stops.py
+++ 787-cheapest-flights-within-k-stops/787. Cheapest Flights Within K Stops.py
@@ -1,27 +1,20 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # diasktra with minHeap picking the next location
-        # no negative cost edge, time:O(N) space:O(M) M is size of the edge
-        edge = DefaultDict(dict)
-        for connect in flights:
-            fromA, toB, price = connect
-            edge[fromA][toB] = price
-        minHeap = []
-        heapq.heappush(minHeap, [0, 0, src])
-        costReach = {}
-        while minHeap:
-            nowCost, connectCnt, nowI = heapq.heappop(minHeap)
-            if nowI in costReach:
-                step, cost = costReach[nowI]
-                if connectCnt>step:
-                    continue
-                
-            costReach[nowI] = [connectCnt, nowCost]
-            if nowI == dst:
-                return nowCost
-            if connectCnt <= k:
-                for nextI, cost in edge[nowI].items():
-                    heapq.heappush(minHeap, [nowCost+cost, connectCnt+1, nextI])
-        return -1
-                        
-        
+        # bellman ford, relax the edges for k+1 times
+        # time:O((N+E)*K) space:O(N)
+        # We are iterating over all the edges K+1 times which takes O(E⋅K). 
+        # At the start and end of each iteration, we also swap distance arrays, 
+        # which take O(N⋅K) time for all the iterations. 
+        reCost = {}
+        reCost[src] = 0
+        for i in range(k+1):
+            newCost = copy.deepcopy(reCost)
+            for flight in flights:
+                fromS, toD, cost = flight
+                if fromS in reCost:
+                    nextC = reCost[fromS]+cost
+                    if toD not in newCost or newCost[toD]>nextC:
+                        newCost[toD] = nextC
+            reCost = newCost
+        # print(reCost)
+        return reCost[dst] if dst in reCost else -1
