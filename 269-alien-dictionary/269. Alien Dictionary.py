e76e1deefbd3594bf309bda00715db7b6c83c5a0
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-26 09:42:16 -0800

    Runtime: 4 ms (14.53%), Memory: 17.2 MB (6.32%) - Gitcode

--- 269-alien-dictionary/269. Alien Dictionary.py
+++ 269-alien-dictionary/269. Alien Dictionary.py
@@ -1,37 +1,42 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        # using tree to record the sequence
-        # maintain the root nodes and edge of parent -> child
-        # if a cycle appear meaning we have conflict order
-        adj_list = defaultdict(set)
-        in_degree = Counter({c: 0 for word in words for c in word})
-
-        for word1, word2 in zip(words, words[1:]):
-            for a, b in zip(word1, word2):
+        # need to make connections of the words
+        # creating a tree based on the word will take (n-1) pairs, with each l size => O(NL)
+        # getting the order can be done be inorder degree of each character =>O(L) 
+        # overall time is O(NL)
+        # space:O(N+L) for connections and unique characters
+        # since the unique char is fixed to be 26, the time:O(M) of M is the total length of all words
+        # space:O(U+min(U^2,N)) U is the unique char with N of words = O(U) = O(1)
+        charConnect = defaultdict(set)
+        charIndegree = {c:0 for w in words for c in w}
+        # print(charIndegree)
+        # in_degree = Counter({c: 0 for word in words for c in word})
+        # print(in_degree)
+        for w1, w2 in zip(words, words[1:]):
+            for a, b in zip(w1, w2):
                 if a!=b:
-                    if b not in adj_list[a]:
-                        adj_list[a].add(b)
-                        # record how many parents of b
-                        in_degree[b] += 1
+                    # add the linakge
+                    if b not in charConnect[a]:
+                        charConnect[a].add(b)
+                        charIndegree[b]+=1
                     break
             else:
-                if len(word1)>len(word2):
-                    return""
+                # making sure if b is not a prefix of a
+                if len(w1)>len(w2):
+                    # print(w1, w2)
+                    return ""
+        # print(charIndegree)
+        # print(charConnect)
+        startQ = [k for k in charIndegree.keys() if charIndegree[k] == 0]
+        # print(startQ)
         ret = []
-        # start with the degree of 0 to traverse, bfs
-        queue = deque([c for c in in_degree if in_degree[c] == 0])
-        while queue:
-            now = queue.popleft()
+        while startQ:
+            now = startQ.pop(0)
             ret.append(now)
-            for b in adj_list[now]:
-                in_degree[b] -= 1
-                # only the parents are removed, will this node be added
-                if in_degree[b] == 0:
-                    queue.append(b)
-        # if there's more char in the in_degree, meaning having cycle for conflict
-        # print(adj_list)
-        if len(ret)<len(in_degree):
-            return ""
-        return "".join(ret)
-
-        
+            for nextchar in charConnect[now]:
+                charIndegree[nextchar] -= 1
+                if charIndegree[nextchar] == 0:
+                    startQ.append(nextchar)
+        # print(ret)
+        retStr = "".join(ret)
+        return "" if len(ret)<len(charIndegree.keys()) else retStr
