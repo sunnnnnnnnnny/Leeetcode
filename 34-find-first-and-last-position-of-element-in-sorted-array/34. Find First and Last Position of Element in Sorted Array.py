2143a66ad478be88fbb7c1b9c038fe9bc1c75132
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-27 09:34:06 -0800

    Runtime: 0 ms (100.00%), Memory: 18.6 MB (7.38%) - Gitcode

--- 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34. Find First and Last Position of Element in Sorted Array.py
@@ -1,33 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # 2 binary search for one to get the start, one to get the end
-        # time:O(2LogN) space:O(1)
-        def startSearch(tar, nums, l, r):
+        # binary search
+        def leftSearch(tar, l, r, nlist):
             while l<=r:
                 mid = (l+r)//2
-                if nums[mid] == tar and ((mid>0 and nums[mid-1]<nums[mid]) or mid == 0):
-                    return mid
-                if nums[mid]>=tar:
+                if nlist[mid]>=tar:
                     r = mid-1
                 else:
                     l = mid+1
             return l
-        def endSearch(tar, nums, l, r, end):
+        
+        def rightSearch(tar, l, r, nlist):
             while l<=r:
                 mid = (l+r)//2
-                if nums[mid] == tar and ((mid<end and nums[mid+1]>nums[mid]) or mid == end):
-                    return mid
-                if nums[mid]<=tar:
-                    l = mid+1
+                if nlist[mid]<=tar:
+                    l=mid+1
                 else:
                     r = mid-1
             return r
-        end = len(nums)-1
-        if end < 0:
-            return [-1,-1]
-        start = startSearch(target, nums, 0, end)
-        # print(start)
-        if start<0 or start>end or target != nums[start]:
+        n = len(nums)
+        start = leftSearch(target, 0, n-1, nums)
+        # print(start, n)
+        if start<0 or start>=n or nums[start]!=target:
             return [-1, -1]
-        end = endSearch(target, nums, start, end, end)
+        end = rightSearch(target, start, n-1, nums)
         return [start, end]
