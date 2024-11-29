31123946465a5d304c260254fa3384fd136a58c5
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-11-29 00:45:57 -0800

    Runtime: 0 ms (100.00%), Memory: 17.3 MB (18.59%) - Gitcode

--- 366-find-leaves-of-binary-tree/366. Find Leaves of Binary Tree.py
+++ 366-find-leaves-of-binary-tree/366. Find Leaves of Binary Tree.py
@@ -6,23 +6,19 @@
 #         self.right = right
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # count the level from the end, then add them 
-        retList = []
+        # count the depth from leaf, then added them in the ret
+        dep2Node = defaultdict(list)
         def dfs(now):
-            nonlocal retList
+            nonlocal dep2Node
             if now == None:
-                return -1
-            lEnd = dfs(now.left)
-            rEnd = dfs(now.right)
-            nowEnd = max(lEnd, rEnd)+1
-            if nowEnd == len(retList):
-                retList.append([now.val])
-            else:
-                retList[nowEnd].append(now.val)
-            return nowEnd
-        maxLen = dfs(root)
-        # ret = []
-        # for x in retList:
-        #     ret.extend(x)
-        return retList
-
+                return 0
+            leftDep = dfs(now.left)+1
+            rightDep = dfs(now.right)+1
+            nowDep = max(leftDep, rightDep)
+            dep2Node[nowDep].append(now.val)
+            return nowDep
+        maxDep = dfs(root)
+        ret = []
+        for i in range(1, maxDep+1):
+            ret.append(dep2Node[i])
+        return ret
