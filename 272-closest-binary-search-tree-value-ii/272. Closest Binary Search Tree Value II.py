35cf90d263272a87397b2a202a2a7d2955b19c19
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-12-02 10:10:32 -0800

    Runtime: 0 ms (100.00%), Memory: 19.1 MB (13.92%) - Gitcode

--- 272-closest-binary-search-tree-value-ii/272. Closest Binary Search Tree Value II.py
+++ 272-closest-binary-search-tree-value-ii/272. Closest Binary Search Tree Value II.py
@@ -8,7 +8,7 @@ class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
         # pre-order traversal with list size k, as we reached k items 
         # compare to remove the head or the tail
-        # time:O(N) space:O(N+N) = O(N)
+        # time:O(N+(N-K)) space:O(N+N) = O(N)
         kList = []
         def preOrder(now):
             nonlocal kList, target, k
@@ -16,14 +16,17 @@ class Solution:
                 return 
             preOrder(now.left)
             kList.append(now.val)
+            while len(kList) > k:
+                headDiff = abs(kList[0]-target)
+                tailDiff = abs(kList[-1]-target)
+                # print(kList[0], kList[-1], headDiff, tailDiff)
+                if headDiff>tailDiff:
+                    kList.pop(0)
+                else:
+                    # any further node will be even larger, meaning it's farer from target
+                    # no need to go to other nodes
+                    kList.pop()
+                    return
             preOrder(now.right)
         preOrder(root)
-        while len(kList) > k:
-            headDiff = abs(kList[0]-target)
-            tailDiff = abs(kList[-1]-target)
-            # print(kList[0], kList[-1], headDiff, tailDiff)
-            if headDiff>tailDiff:
-                kList.pop(0)
-            else:
-                kList.pop()
         return kList
