7c3d235255ca490b1698a28ba1e64fa4e5eaee29
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-12-03 21:40:24 -0800

    Runtime: 125 ms (83.37%), Memory: 71.1 MB (5.54%) - Gitcode

--- 716-max-stack/716. Max Stack.py
+++ 716-max-stack/716. Max Stack.py
@@ -1,84 +1,56 @@
 class MaxStack:
-# using 2 struct to store, the queue as regular, max stack for the max
-# keep record if it's earse from the max stack
-# time:O() space:O(2N)
+# getting the max elemtent with stack FILO
+# need unique identify for each elementt by a separte dict to record the exsistence
+# having maxHeap for the retriving the max
+# list for regular stack operation
+# counter for identifier
+# space:O(3N) = O(N)
     def __init__(self):
         self.cnt = 0
-        self.maxS = []
-        self.Q = []
-        self.sErase = {}
-
+        self.maxH = []
+        self.stack = []
+        self.exist = set()
+# t:O(logN)
     def push(self, x: int) -> None:
-        # time:O(1)
-        self.Q.append((x,self.cnt))
-        heapq.heappush(self.maxS, (-x,-self.cnt))
+        kid = self.cnt
+        heapq.heappush(self.maxH, (-x, -kid))
+        self.stack.append((x, kid))
+        self.exist.add(kid)
         self.cnt += 1
-
+# t:O(logN)
     def pop(self) -> int:
-        # time:O(N)
-        now = self.Q.pop()
-        ret = now[0]
-        findI = now[1]
-        while findI>=0:
-            ret = now[0]
-            findI = now[1]
-            if findI in self.sErase.keys():
-                del self.sErase[findI]
-                now = self.Q.pop()
-            else:
-                self.sErase[findI] = now
-                break
-        return ret
-        
+        while self.stack:
+            val, kid = self.stack.pop()
+            if kid in self.exist:
+                self.exist.remove(kid)
+                return val
+        return 0
+# t:O(1)   because the remove might never happens, amortized time would be O(1)
     def top(self) -> int:
-        # time:O(N)
-        now = self.Q[-1]
-        ret = now[0]
-        findI = now[1]
-        while findI>=0:
-            now = self.Q[-1]
-            ret = now[0]
-            findI = now[1]
-            if findI in self.sErase.keys():
-                del self.sErase[findI]
-                self.Q.pop()
+        while self.stack:
+            val, kid = self.stack[-1]
+            if kid not in self.exist:
+                self.stack.pop()
             else:
-                break
-        return ret
-
+                return val
+        return 0
+# t:O(logN)
     def peekMax(self) -> int:
-        # time:O(NlogN)
-        now = self.maxS[0]
-        ret = now[0]
-        while self.maxS:
-            now = self.maxS[0]
-            idx = -now[1]
-            ret = now[0]
-            if idx in self.sErase.keys():
-                heapq.heappop(self.maxS)
+        while self.maxH:
+            val, kid = self.maxH[0]
+            if -kid not in self.exist:
+                heapq.heappop(self.maxH)
             else:
-                break
-                
-        return -ret
-    
+                return -val
+        return 0
+# t:O(logN)
     def popMax(self) -> int:
-        # time:O(NlogN)
-        # print(self.maxS)
-        # print(self.sErase)
-        now = self.maxS[0]
-        ret = now[0]
-        while self.maxS:
-            now = self.maxS[0]
-            idx = -now[1]
-            ret = now[0]
-            if idx in self.sErase.keys():
-                heapq.heappop(self.maxS)
-            else:
-                self.sErase[idx] = -ret
-                heapq.heappop(self.maxS)
-                break
-
-        return -ret
+        while self.maxH:
+            val, kid = heapq.heappop(self.maxH)
+            if -kid in self.exist:
+                self.exist.remove(-kid)
+                return -val
+        return 0
         
 
 
