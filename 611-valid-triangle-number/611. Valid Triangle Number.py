f91e48ec6ffff7983154ba58bbbcabe2c84b132b
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-12-04 00:52:46 -0800

    Runtime: 737 ms (31.00%), Memory: 17.6 MB (6.13%) - Gitcode

--- 611-valid-triangle-number/611. Valid Triangle Number.py
+++ 611-valid-triangle-number/611. Valid Triangle Number.py
@@ -5,20 +5,26 @@ class Solution:
         # we can sort the array from small to large
         # then pick the first 2 num, and dirctly getting the possible large side
         # time:O(NlogN+N^3) space:O(N)
-        # if we get the thrid side with binary search
+        # if we get the thrid side with binary search time:O(N^2logN)
         nums.sort()
         n = len(nums)
         ret = 0
+        # for moving j and k at most n in the for, time:O(NlogN+N^2)
+        # space:O(N) for sorting
         for f in range(n-2):
-            for s in range(f+1, n-1):
-                largeS = nums[f]+nums[s]
-                tIdx = bisect.bisect_left(nums, largeS)
-                # print(tIdx, largeS)
-                if tIdx>s and nums[tIdx-1]<largeS:
-                    ret += tIdx-s-1
-                # for t in range(s+1, n):
-                #     if nums[t]<largeS:
-                #         ret += 1
-                #     else:
-                #         break
+            k = f+2
+            if nums[f]==0:
+                continue
+            for j in range(f+1, n-1):
+                while k<n and nums[f]+nums[j]>nums[k]:
+                    k+=1
+                ret += k-j-1
+        
+        # for f in range(n-2):
+        #     for s in range(f+1, n-1):
+        #         largeS = nums[f]+nums[s]
+        #         tIdx = bisect.bisect_left(nums, largeS)
+        #         # print(tIdx, largeS)
+        #         if tIdx>s and nums[tIdx-1]<largeS:
+        #             ret += tIdx-s-1
         return ret
