f80628f5da715f61e561b60f2849d23b6a7dc518
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-12-03 20:42:37 -0800

    Runtime: 24 ms (87.28%), Memory: 17.6 MB (10.28%) - Gitcode

--- 1004-max-consecutive-ones-iii/1004. Max Consecutive Ones III.py
+++ 1004-max-consecutive-ones-iii/1004. Max Consecutive Ones III.py
@@ -1,19 +1,19 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        # since it's consectutive then we use sliding window to count the 0 and 1
-        # if the 0 cnt is less than k, then it's valid
-        # always keep the expanding right while keeping left valid
-        # time:O(N) space:O(1)
-        left = 0
-        ret = 0
-        oneCnt = zeroCnt = 0
-        for right in range(len(nums)):
-            if nums[right] == 0:
-                zeroCnt += 1
-                while zeroCnt>k:
-                    if nums[left] == 0:
-                        zeroCnt -= 1
-                    left += 1
-            now = right-left+1
-            ret = max(ret, now)
-        return ret
+        # for consecutive ones it's a sliding window, 
+        # by having the cnt of 0 in the window to determine the window is valid
+        # time:O(N) space:O(1) just the count of 0 in the string
+        start = 0
+        zCnt = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zCnt+=1
+            if zCnt>k:
+                # always keep the maxValidWindowSize
+                if nums[start] == 0:
+                    zCnt -=1
+                start += 1
+        
+        nowLen = len(nums)-start
+        return nowLen
+            
