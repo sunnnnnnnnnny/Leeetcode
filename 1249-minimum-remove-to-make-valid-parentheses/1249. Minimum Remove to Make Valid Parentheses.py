d89f022e0fcad61f6d8805f993477c72d03b94df
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-12-09 23:46:51 -0800

    Runtime: 71 ms (15.89%), Memory: 19 MB (25.33%) - Gitcode

--- 1249-minimum-remove-to-make-valid-parentheses/1249. Minimum Remove to Make Valid Parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249. Minimum Remove to Make Valid Parentheses.py
@@ -1,32 +1,31 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        # by greedyly match the valid match
-        # everytime the find the valid pair, then pop it to make the sentence
-        # time:O(N) space:O(N)
-        openCnt = 0
-        retArr = []
-        for i in range(len(s)):
-            if s[i] == '(':
-                openCnt+=1
-                retArr.append(s[i])
-            elif s[i] == ')':
-                if openCnt==0:
-                    continue
-                openCnt -= 1
-                mergeW = s[i]
-                while retArr:
-                    now = retArr.pop()
-                    mergeW = now+mergeW
-                    if now == '(':
-                        break
-                retArr.append(mergeW)
-            else:
-                retArr.append(s[i])
+        # using a queue for each left pos, if the right can make left a pair
+        # then the string will be valid until the next left
+        # time:O(2N) space:O(N)
+        leftQ = []
+        leftCnt = 0
         ret = ""
-        while retArr:
-            now = retArr.pop()
-            # any left over ( should be ignored
+        # print(s, len(s))
+        for i in range(len(s)):
+            # print(i, leftCnt, leftQ)
+            if s[i] == ')':
+                if leftCnt>0:
+                    leftQ.append(s[i])
+                    while leftQ:
+                        now = leftQ.pop(0)
+                        ret += now
+                        if now == '(':
+                            leftCnt -= 1
+                            break
+            else:
+                if s[i] == '(':
+                    leftCnt+=1
+                leftQ.append(s[i])
+        while leftQ:
+            now = leftQ.pop(0)
             if now == '(':
                 continue
-            ret = now+ret
+            ret += now
         return ret
+                
