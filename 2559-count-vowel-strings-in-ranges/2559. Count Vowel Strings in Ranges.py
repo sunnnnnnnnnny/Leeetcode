e9e152339033e8ef71e957f818304cd08a7d2e4f
sunnnnnnnnnny <40786952+sunnnnnnnnnny@users.noreply.github.com>
2024-12-21 18:12:33 -0800

    Runtime: 39 ms (7.49%), Memory: 49.6 MB (39.61%) - Gitcode

---
+++ 2559-count-vowel-strings-in-ranges/2559. Count Vowel Strings in Ranges.py
@@ -0,0 +1,20 @@
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        # precalculate the counts and using the range to directly deducute
+        # time:O(N+M) n is words, m is query
+        vowelCnt = 0
+        def isSEvow(w):
+            if w[0] in set(["a",'e','i','o','u']) and w[-1] in set(["a",'e','i','o','u']):
+                return True
+            return False
+        n = len(words)
+        wCnt = [0]*(n+1)
+        for i in range(n):
+            if isSEvow(words[i]):
+                vowelCnt+=1
+            wCnt[i+1] = vowelCnt
+        ret = []
+        for q1, q2 in queries:
+            nowRet = wCnt[q2+1]-wCnt[q1]
+            ret.append(nowRet)
+        return ret
